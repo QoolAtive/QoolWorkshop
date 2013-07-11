9b906e895525d6cd7833dfb09d17deff7bb46a08
golfiti <golfitiz@gmail.com>
2013-07-11 16:54:28 +0700

    จัดความสูงหน้าแรก

--- protected/views/layouts/main_index.php
+++ protected/views/layouts/main_index.php
@@ -1,57 +1,76 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html lang="en" class="no-js">
-    <head>
-        <title>DBDmart.com</title>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8"></meta>
-        <meta charset="UTF-8"></meta>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-        <link rel="shortcut icon" href="favicon.ico" /></link>
+<head>
+    <title>DBDmart.com</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"></meta>
+    <meta charset="UTF-8"></meta>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <link rel="shortcut icon" href="favicon.ico" /></link>
 
-        <!--CSS-->
-        <link rel="stylesheet" href="/css/global.css" type="text/css"></link>
-        <link rel="stylesheet" href="/css/style.css" type="text/css"></link>
-        <link rel="stylesheet" href="/css/animate.css" type="text/css"></link>
-        <link rel="stylesheet" href="/css/jquery.fancybox.css" type="text/css" media="screen" /></link>
+    <!--CSS-->
+    <link rel="stylesheet" href="/css/global.css" type="text/css"></link>
+    <link rel="stylesheet" href="/css/style.css" type="text/css"></link>
+    <link rel="stylesheet" href="/css/animate.css" type="text/css"></link>
+    <link rel="stylesheet" href="/css/jquery.fancybox.css" type="text/css" media="screen" /></link>
 
-        <!--JS-->
-        <!--[if IE]>
-        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]--> 
-        <script src="/js/jquery-1.9.0.min.js" type="text/javascript"></script>
-        <script src="/js/modernizr.js" type="text/javascript"></script>
-        <script src="/js/jquery.fancybox.js" type="text/javascript"></script>
-        <script src="/js/tabcontent.js" type="text/javascript"></script>
-        <script src="/js/config.js" type="text/javascript"></script>
-    </head>
-    <body class="bgindex">
+    <!--JS-->
+<!--[if IE]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]--> 
+<script src="/js/jquery-1.9.0.min.js" type="text/javascript"></script>
+<script src="/js/modernizr.js" type="text/javascript"></script>
+<script src="/js/jquery.fancybox.js" type="text/javascript"></script>
+<script src="/js/tabcontent.js" type="text/javascript"></script>
+<script src="/js/config.js" type="text/javascript"></script>
 
-        <!--wrapper-->
-        <div id="wrapper" class="page pagepadding white pagecen pageborder">
+<style type="text/css">
 
-            <!--#header-->
-            <div id="header" class="headindex clearfix">
-                <?php
-                $this->renderPartial("//layouts/_index-logo");
-                $this->renderPartial("//layouts/_index-menu_header")
-                ?>           
+
+#container {
+
+      padding: 3% 0 !important;
+    position: absolute;
+    top: 1%;
+    height: auto;
+}
+
+</style>
+</head>
+<body class="bgindex">
+
+    <!--wrapper-->
+    <div id="wrapper" class="page pagepadding white pagecen pageborder">
+
+
+        <!--#container-->
+        <div id="container" class="page clearfix">
+            <div class="clearfix" style=" height:20%;  padding-bottom: 25px;">
+
+                <div id="header" class="headindex clearfix">
+
+
+                    <?php
+                    $this->renderPartial("//layouts/_index-logo");
+                    $this->renderPartial("//layouts/_index-menu_header");
+                    ?>  
+
+                </div>
 
             </div>
-            <!--/#header-->
-
-            <!--#container-->
-            <div id="container" class="page clearfix smart"  >
+            <div style=" height:80%;" class="smart clearfix">
                 <?php echo $content; ?>
-            </div><!-- container -->
-            <div id="push"></div>
+            </div>
 
-        </div>
-        <!--/#wrapper-->
+        </div><!-- container -->
 
-        <!--#footer-->
-        <div id="footer" class="clearfix">
-            <?php $this->renderPartial("//layouts/_index-footer"); ?>
-        </div>
-    </body>
+    </div>
+    <!--/#wrapper-->
+
+    <!--#footer-->
+    <div id="footer" class="clearfix">
+        <?php $this->renderPartial("//layouts/_index-footer"); ?>
+    </div>
+</body>
 </html>
