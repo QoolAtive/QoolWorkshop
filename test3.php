81dca83f2f40f8d532a07a525774b2dfa2e86a91
Piyanan <pyn@live.com>
2013-07-12 12:38:24 +0700

    rs001-01_1

--- test3.php
+++ test3.php
@@ -1,6 +1,8 @@
 <?php
-echo "eeeeeeeeeeeee";
 echo "Qoolative";
+echo "Qoolative";
+echo "Qoolative";
+echo "aaaaaa";
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
