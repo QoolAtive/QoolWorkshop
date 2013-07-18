20df71d4173d09cd068c9d045c28ea24480fc58c
annbluesky <ann.kanowan@gmail.com>
2013-07-18 15:49:11 +0700

    Update Language ส่วนชื่อ Menu

--- protected/messages/en/language.php
+++ protected/messages/en/language.php
@@ -3,13 +3,23 @@
 return array(
     'language' => 'en',
     'DBD' => 'DBD',
+    'Knowledge & Learning' => 'Knowledge & Learning.',
+    'Web Simulation' => 'Web Simulation.',
+    'E-Directory' => 'E-Directory.',
+    'Service Provider' => 'Service Provider.',
+    'Link' => 'Link.',
+    'FAQ' => 'FAQ',
+    'About Us' => 'About Us',
+    'News & Activity' => 'News & Activity',
+    '' => '',
     'link.png' => 'link_en.png',
-    'linkbanner.png' => 'linkbanner_en.png',
+    '/img/banner/link.png' => '/img/banner/link_en.png',
     'จัดการลิงค์' => 'Manage Link',
+    'ค้นหา' => 'Search',
+    'ค้นหาตามชื่อลิงก์' => 'Search by link name.',
+    'ค้นหาตามกลุ่มลิงก์' => 'Search by link group.',
+    '' => '',
     'Knowledge' => 'Knowledge',
-    'jjhhhh' => '',
-    '' => '',
-    '' => '',
-    '' => '',
+    
 );
 ?>
