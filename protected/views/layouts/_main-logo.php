c0b4d5e0b9697591cdc30f610e7f7281edf5c1e1
vasitjuntong <N_vasit@hotmail.com>
2013-07-18 15:00:52 +0700

    layout_แก้ลิ้ง โลโก้ และ ชื่อสมาชิกตอน login

--- protected/views/layouts/_main-logo.php
+++ protected/views/layouts/_main-logo.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
     <ul class="logo">
         <li class="logopage">
-            <a href="http://dbd.qoolative.com"><img src="/img/logo.png"></a>
+            <a href="/site/index"><img src="/img/logo.png"></a>
         </li>
     </ul>
 </div>
