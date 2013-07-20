47f9c72383f15605a2a15834f897386787fdc28c
golfiti <golfitiz@gmail.com>
2013-07-20 15:31:06 +0700

    เอา config fullcalendar ออก

--- js/config.js
+++ js/config.js
@@ -13,10 +13,8 @@ $(function() {
 
 $(document).ready(function() {
      $('.linkgroupbtn').fancybox({
-        // content: $("#divForm").html()
-        autoSize:true,
-        autoWidth:true,
-        width :550
+         width  :'100%',
+         closeBtn : 0
         // afterClose : function() {
         //         location.reload();
         //         return;
@@ -223,70 +221,7 @@ $(document).ready(function() {
 
 
 // Full Calendar
-$(document).ready(function() {
-    
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-        
-    $('#calendar').fullCalendar({
-        header: {
-            left: 'title',
-            center: '',
-            right: 'month,basicWeek,basicDay prev,next today'
-        //                                right: 'month,basicWeek,basicDay prev,next today'
-        },
-        editable: true,
-        events: [
-        {
-            title: 'All Day Event',
-            start: new Date(y, m, 1)
-        },
-        {
-            title: 'Long Event',
-            start: new Date(y, m, d-5),
-            end: new Date(y, m, d-2)
-        },
-        {
-            id: 999,
-            title: 'Repeating Event',
-            start: new Date(y, m, d-3, 16, 0),
-            allDay: false
-        },
-        {
-            id: 999,
-            title: 'Repeating Event',
-            start: new Date(y, m, d+4, 16, 0),
-            allDay: false
-        },
-        {
-            title: 'Meeting',
-            start: new Date(y, m, d, 10, 30),
-            allDay: false
-        },
-        {
-            title: 'Lunch',
-            start: new Date(y, m, d, 12, 0),
-            end: new Date(y, m, d, 14, 0),
-            allDay: false
-        },
-        {
-            title: 'Birthday Party',
-            start: new Date(y, m, d+1, 19, 0),
-            end: new Date(y, m, d+1, 22, 30),
-            allDay: false
-        },
-        {
-            title: 'Click for Google',
-            start: new Date(y, m, 28),
-            end: new Date(y, m, 29),
-            url: 'http://google.com/'
-        }
-        ]
-    });
-        
-});
+
 
 
 
