1a29e6914ea0a8c13df9634716c3c8a9087ee476
annbluesky <ann.kanowan@gmail.com>
2013-07-20 11:17:12 +0700

    ลืมใส่ ; หน้าเกี่ยวกับเรา

--- protected/modules/about/views/default/index.php
+++ protected/modules/about/views/default/index.php
@@ -58,7 +58,7 @@
             <div id="text" class="row-fluid ">
                 <?php
                 $model_text = About::model()->find();
-                echo languageHelper::changeDB($model_text->about_text_th, $model_text->about_text_en)
+                echo languageHelper::changeDB($model_text->about_text_th, $model_text->about_text_en);
                 ?>
             </div>
         </div>
