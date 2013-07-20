d9951b0765af3f123d1abeb89891e73d135ebcd1
annbluesky <ann.kanowan@gmail.com>
2013-07-20 10:48:25 +0700

    origin_helper for language

---
+++ protected/helpers/languageHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * languageHelper Create By Ann
+ * Add Date : 20-07-56
+ * Update Date : 20-07-56
+ */
+
+class languageHelper {
+
+    /*
+     * แปลงค่าให้ดึงข้อมูลจาก Database ให้ถูกตามภาษาที่เลือก
+     * 
+     * @param   text $value_th ข้อความภาษาไทย
+     *          text $value_en ข้อความภาษาอังกฤษ
+     * @return  text ข้อความที่เปลี่ยนตามภาษาที่เลือก
+     * 
+     * Add Date : 20-07-56
+     * Update Date : 20-07-56
+     */
+
+    static public function changeDB($value_th, $value_en) {
+        $currentLang = Yii::app()->language;
+        $value = $value_th;
+        if ($currentLang == 'en') {
+            if ($value_en != '') {
+                $value = $value_en;
+            }
+        }
+        return $value;
+    }
+
+}
+
+?>
