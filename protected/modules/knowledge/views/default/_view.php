d851ad4dd9286c53ecf702f2c0006ad497b82e1d
annbluesky <ann.kanowan@gmail.com>
2013-07-20 14:43:15 +0700

    หน้าบทความ (Knowledge) ส่วนเนื้อหา เรียก ข้อมูลจาก field ใน Database จาก  Helper

--- protected/modules/knowledge/views/default/_view.php
+++ protected/modules/knowledge/views/default/_view.php
@@ -1,43 +1,47 @@
 <div class="sidebar">
     <div class="menuitem">
         <ul>
-            <li class="boxhead"><img src="/img/iconpage/knowledge.png"/></li>
+            <li class="boxhead"><img src="<?php echo Yii::t('language', '/img/iconpage/knowledge.png'); ?>"/></li>
         </ul>
         <ul class="tabs clearfix">
             <?php
             $list = array(
-                array('text' => 'เนื้อข่าว', 'link' => '#',),
-                array('text' => 'ข่าวทั้งหมด', 'link' => '#',),
+                array('text' => Yii::t('language', 'เนื้อหา'), 'link' => '#',),
+                array('text' => Yii::t('language', 'บทความทั้งหมด'), 'link' => '#',),
             );
             $n = 1;
             foreach ($list as $ls) {
                 echo "<li>" . CHtml::link($ls['text'], $ls['link'], array('rel' => 'view' . $n++)) . "</li>";
             }
             $list2 = array(
-                array('text' => 'Knowledge', 'link' => '/knowledge/default/index'),
-                array('text' => 'Learning', 'link' => '/knowledge/default/index#view2'),
+                array('text' => Yii::t('language', 'บทความ'), 'link' => '/knowledge/default/index'),
+                array('text' => Yii::t('language', 'การเรียนรู้'), 'link' => '/knowledge/default/index#view2'),
             );
             $n2 = 3;
             foreach ($list2 as $ls) {
                 echo "<li>" . CHtml::link($ls['text'], $ls['link'], array('rel' => 'view' . $n2++)) . "</li>";
             }
 //            echo Tool::GenList($list);
-            ?> 
+            if (Yii::app()->user->isAdmin()) {
 
-            <li>
-                <?php
-                if (Yii::app()->user->isAdmin()) {
-                    echo CHtml::link('Manage Knowledge', array('/knowledge/manage/knowledge'), array('rel' => 'view5'));
-                }
-                ?>
-            </li>
-            <li> 
-                <?php
-                if (Yii::app()->user->isAdmin()) {
-                    echo CHtml::link('Manage Learning', array('/learning/manage/learning'), array('rel' => 'view6'));
-                }
-                ?>
-            </li>
+                echo "<li>";
+                echo CHtml::link(
+                        Yii::t('language', 'จัดการ') . " " . Yii::t('language', 'บทความ'), array(
+                    '/knowledge/manage/knowledge'
+                        ), array(
+                    'rel' => 'view5'
+                ));
+                echo "</li>";
+                echo "<li>";
+                echo CHtml::link(
+                        Yii::t('language', 'จัดการ') . " " . Yii::t('language', 'การเรียนรู้'), array(
+                    '/learning/manage/learning'
+                        ), array(
+                    'rel' => 'view6'
+                ));
+                echo "</li>";
+            }
+            ?> 
         </ul>
     </div>
 </div>
@@ -45,34 +49,35 @@
     <div class="tabcontents">
         <div id="view1" class="tabcontent ">
             <div class="knowledgeview">
-
                 <!-- <div class="btnedit"> -->
                 <?php
-                if (Yii::app()->user->isAdmin())
-                    echo CHtml::button('แก้ไข', array('class' => "btnedit grey", 'onClick' => "window.location='" . CHtml::normalizeUrl(array(
+                if (Yii::app()->user->isAdmin()) {
+                    echo CHtml::button(
+                            Yii::t('language', 'แก้ไข'), array(
+                        'class' => "btnedit grey",
+                        'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                             '/knowledge/manage/insert/id/' . $view->id
                         )) . "'")
                     );
-                ?>
-                <!-- </div>
-                -->
+                }
+                $subject = LanguageHelper::changeDB($view->subject, $view->subject_en);
+                $detail = LanguageHelper::changeDB($view->detail, $view->detail_en);
+                ?>  
                 <div style="text-align: center;">
                     <img src="/file/knowledge/<?php echo $view->image; ?>" style="height: 300px; max-width: 748px;" />
                 </div>
-
                 <div class="knowledgeviewtext">
-
                     <h3>
                         <img src="/img/iconform.png" alt="icon"/>
                         <?php
-                        echo $view->subject;
+                        echo $subject;
                         ?>
                     </h3>
-                    <p><?php echo $view->detail; ?></p>
+                    <p><?php echo $detail; ?></p>
 
                     <div style="text-align: center; margin-top:10px;">
                         <?php
-                        echo CHtml::button('ย้อนกลับ', array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+                        echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
                                 Yii::app()->user->getState('link_back')
                             )) . "'")
                         );
