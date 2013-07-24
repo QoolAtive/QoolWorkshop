bd40d776a7746ef286112ed89aa9aab65c2dc90d
Vasit <Vasit@Vasit-PC>
2013-07-24 18:25:45 +0700

    model SpCompany_เพิ่มตัวแปล type_id

--- protected/models/SpCompany.php
+++ protected/models/SpCompany.php
@@ -2,13 +2,15 @@
 
 class SpCompany extends SpCompanyBase {
 
+    public $type_id;
+    
     public static function model($className = __CLASS__) {
         return parent::model($className);
     }
 
     public function rules() {
         return array(
-            array('name, name_en, infor, infor_en, address, address_en, contact_name, contact_name_en, contact_tel, contact_fax, contact_email, website, partner', 'required'),
+            array('name, name_en, infor, infor_en, address, address_en, contact_name, contact_name_en, contact_tel, contact_fax, contact_email, website', 'required'),
             array('type_business', 'numerical', 'integerOnly' => true),
             array('logo, contact_name, contact_name_en, contact_tel, contact_fax, contact_email, twitter, banner, brochure', 'length', 'max' => 100),
             array('name, name_en, address, address_en, facebook, website', 'length', 'max' => 255),
@@ -73,6 +75,43 @@ class SpCompany extends SpCompanyBase {
     public function getData() {
         $criteria = new CDbCriteria;
         $criteria->order = 'id desc';
+        $criteria->compare('id', $this->id);
+        $criteria->compare('logo', $this->logo, true);
+        $criteria->compare('name', $this->name, true);
+        $criteria->compare('name_en', $this->name_en, true);
+        $criteria->compare('infor', $this->infor, true);
+        $criteria->compare('infor_en', $this->infor_en, true);
+        $criteria->compare('type_business', $this->type_business);
+        $criteria->compare('address', $this->address, true);
+        $criteria->compare('address_en', $this->address_en, true);
+        $criteria->compare('contact_name', $this->contact_name, true);
+        $criteria->compare('contact_name_en', $this->contact_name_en, true);
+        $criteria->compare('contact_tel', $this->contact_tel, true);
+        $criteria->compare('contact_email', $this->contact_email, true);
+        $criteria->compare('facebook', $this->facebook, true);
+        $criteria->compare('twitter', $this->twitter, true);
+        $criteria->compare('website', $this->website, true);
+        $criteria->compare('banner', $this->banner, true);
+        $criteria->compare('brochure', $this->brochure, true);
+        $criteria->compare('partner', $this->partner, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    public function getDataType($type = NULL) {
+
+        $criteria = new CDbCriteria;
+        $criteria->order = 'id desc';
+        $criteria->select = "t.*, stc.type_id as type_id";
+        $criteria->join = "
+            left join sp_type_com stc on t.id = stc.com_id
+            ";
+        if ($type != null) {
+            $criteria->condition = "stc.type_id = '" . $type . "'";
+        }
+
         $criteria->compare('id', $this->id);
         $criteria->compare('logo', $this->logo, true);
         $criteria->compare('name', $this->name, true);
