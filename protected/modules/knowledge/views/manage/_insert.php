e8669ee60b0fa82b19abb4831b4b3076e858f0f4
annbluesky <ann.kanowan@gmail.com>
2013-07-24 17:27:44 +0700

    ใส่ Function แปลภาษา ในหน้าเพิ่มบทความ (Knowledge)

--- protected/modules/knowledge/views/manage/_insert.php
+++ protected/modules/knowledge/views/manage/_insert.php
@@ -1,8 +1,8 @@
 <?php
 if ($model->id != NULL) {
-    $btnText = 'แก้ไขบทความ';
+    $btnText = Yii::t('language', 'แก้ไข') . Yii::t('language', 'บทความ');
 } else {
-    $btnText = 'เพิ่มบทความ';
+    $btnText = Yii::t('language', 'เพิ่ม') . Yii::t('language', 'บทความ');
 }
 ?>
 
@@ -16,66 +16,64 @@ if ($model->id != NULL) {
 $form = $this->beginWidget('CActiveForm', array(
     'id' => 'insert_knowledge-form',
     'htmlOptions' => array('enctype' => 'multipart/form-data'),
-    ));
-    ?>
-    <div class="_100">
-        <h4 class="reg"><?php echo Yii::t('language', '- บทความภาษาไทย -'); ?></h4>
-    </div>
-    <div class="_100">
-        <div class="ckleft">
-            <?php
-            echo $form->label($model, 'subject');
-            ?>
-
-        </div>
-        <div class="ckright">
-            <?php
-            echo $form->textField($model, 'subject');
-            echo $form->error($model, 'subject')
-            ?>
-        </div>
-    </div>
-    <div class="_100">
-
-        <div class="ckleft">
-            <?php   echo $form->label($model, 'detail');?>
-        </div> <div class="ckright">
-        <?php
-
-
-        $this->widget('ext.ckeditor.CKEditorWidget', array(
-"model" => $model, # Data-Model
-"attribute" => 'detail', # Attribute in the Data-Model  // textarea name=""
-"defaultValue" => $model->detail, # Optional
-"config" => array(
-    "resize_dir" => "vertical",
-    "height" => "240px",
-    "width" => "100%",
-    'toolbar' => array(
-        array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
-            '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
-            '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
-        array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
-            '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
-            '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About',),
-), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
-    ),
-"ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
-# Path to ckeditor.php
-"ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
-));
-echo $form->error($model, 'detail');
+        ));
 ?>
-</div>
+<div class="_100">
+    <h4 class="reg"><?php echo "- " . Yii::t('language', 'บทความภาษาไทย') . " -"; ?></h4>
 </div>
 <div class="_100">
-    <h4 class="reg"><?php echo Yii::t('language', '- บทความภาษาอังกฤษ -'); ?></h4>
+    <div class="ckleft">
+        <?php
+        echo $form->label($model, 'subject');
+        ?>
+
+    </div>
+    <div class="ckright">
+        <?php
+        echo $form->textField($model, 'subject');
+        echo $form->error($model, 'subject')
+        ?>
+    </div>
+</div>
+<div class="_100">
+
+    <div class="ckleft">
+        <?php echo $form->label($model, 'detail'); ?>
+    </div> <div class="ckright">
+        <?php
+        $this->widget('ext.ckeditor.CKEditorWidget', array(
+            "model" => $model, # Data-Model
+            "attribute" => 'detail', # Attribute in the Data-Model  // textarea name=""
+            "defaultValue" => $model->detail, # Optional
+            "config" => array(
+                "resize_dir" => "vertical",
+                "height" => "240px",
+                "width" => "100%",
+                'toolbar' => array(
+                    array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
+                        '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
+                        '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
+                    array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
+                        '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
+                        '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About',),
+                ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
+            ),
+            "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
+# Path to ckeditor.php
+            "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
+        ));
+        echo $form->error($model, 'detail');
+        ?>
+    </div>
+</div>
+<div class="_100">
+    <h4 class="reg"><?php echo "- " . Yii::t('language', 'บทความภาษาอังกฤษ') . " -"; ?></h4>
 </div>
 
 
 <div class="_100">
     <div class="ckleft">
-        <?php   echo $form->label($model, 'subject_en');  ?>
+        <?php echo $form->label($model, 'subject_en'); ?>
     </div>
 
     <div class="ckright">
@@ -93,93 +91,92 @@ echo $form->error($model, 'detail');
     </div>
     <div class="ckright">
         <?php
-
         $this->widget('ext.ckeditor.CKEditorWidget', array(
-"model" => $model, # Data-Model
-"attribute" => 'detail_en', # Attribute in the Data-Model  // textarea name=""
-"defaultValue" => $model->detail_en, # Optional
-"config" => array(
-    "resize_dir" => "vertical",
-    "height" => "240px",
-    "width" => "100%",
-    'toolbar' => array(
-        array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
-            '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
-            '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
-        array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
-            '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
-            '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About',),
-), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
-    ),
-"ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
+            "model" => $model, # Data-Model
+            "attribute" => 'detail_en', # Attribute in the Data-Model  // textarea name=""
+            "defaultValue" => $model->detail_en, # Optional
+            "config" => array(
+                "resize_dir" => "vertical",
+                "height" => "240px",
+                "width" => "100%",
+                'toolbar' => array(
+                    array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
+                        '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
+                        '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
+                    array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
+                        '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
+                        '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About',),
+                ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
+            ),
+            "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
 # Path to ckeditor.php
-"ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
-));
-echo $form->error($model, 'detail_en');
-?>
-</div>
+            "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
+        ));
+        echo $form->error($model, 'detail_en');
+        ?>
+    </div>
 </div>
 <div class="_100">
-    
-        <div class="ckleft">
-            <?php
-            echo $form->label($model, 'guide_status');
-            ?>
-        </div>
 
-        <div class="ckright">
-            <?php
-            echo $form->radioButtonList($model, 'guide_status', array('0' => 'ไม่เลือก', '1' => 'เลือก'), array());
-            ?>
-        </div>
-    
+    <div class="ckleft">
+        <?php
+        echo $form->label($model, 'guide_status');
+        ?>
+    </div>
+
+    <div class="ckright">
+        <?php
+        echo $form->radioButtonList($model, 'guide_status', array('0' => 'ไม่เลือก', '1' => 'เลือก'), array());
+        ?>
+    </div>
+
 </div>
 <div class="_100">
-       <div class="ckleft"> 
-            <?php  echo CHtml::label('รูปภาพเก่า', false); ?>
-        </div>
-        <div class="ckright">
+    <div class="ckleft"> 
+        <?php echo CHtml::label(Yii::t('language', 'รูปภาพเดิม'), false); ?>
+    </div>
+    <div class="ckright">
+        <?php
+        if (!empty($model->image)) {
+            ?>
             <?php
-            if (!empty($model->image)) {
-                ?>
-                <?php
-                echo CHtml::image("/file/knowledge/" . $model->image, "image", array('height' => '100'));
-                echo CHtml::label($model->image, false, array('class' => 'hidden'));
-                ?>
-                <?php
-            }
-            ?> 
-        </div>
+            echo CHtml::image("/file/knowledge/" . $model->image, "image", array('height' => '100'));
+            echo CHtml::label($model->image, false, array('class' => 'hidden'));
+            ?>
+            <?php
+        }
+        ?> 
+    </div>
 </div>
 
 <div class="_100"> 
-                    <div class="ckleft">
+    <div class="ckleft">
 
-                        <?php
-                        if (empty($model->image)) {
-                            echo $form->labelEx($model, 'image');
-                        } else {
-                            echo $form->labelEx($file, 'image');
-                        }
-                        ?>
-               
-                    </div>
-                    <div class="ckright">   
-                        <?
-                        echo $form->fileField($file, 'image', array('accept' => 'imaage/*'));
-                        echo $form->error($file, 'image');
-                        ?>
-                    </div>
- </div>
-          
+        <?php
+        if (empty($model->image)) {
+            echo $form->labelEx($model, 'image');
+        } else {
+            echo $form->labelEx($file, 'image');
+        }
+        ?>
+
+    </div>
+    <div class="ckright">   
+        <?
+        echo $form->fileField($file, 'image', array('accept' => 'imaage/*'));
+        echo $form->error($file, 'image');
+        ?>
+    </div>
+</div>
+
 
 
 <div class="_100 textcenter">
     <?php
     echo CHtml::submitButton($btnText);
-    echo CHtml::button('ยกเลิก', array('onClick' => "history.go(-1)")
-        );
-        ?>
+    echo CHtml::button(Yii::t('language', 'ยกเลิก'), array('onClick' => "history.go(-1)")
+    );
+    ?>
 </div>
-    <?php $this->endWidget(); ?>
+<?php $this->endWidget(); ?>
 
