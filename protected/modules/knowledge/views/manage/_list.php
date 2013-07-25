d60dc102d0582722dd6a97e233907f87b22436f3
annbluesky <ann.kanowan@gmail.com>
2013-07-25 11:28:38 +0700

    ใส่ Function แปลภาษา ในหน้าจัดการบทความ (Knowledge) ส่วน Admin เพิ่มเรียก ข้อมูลจาก field ใน Database จาก  Helper (_list)

--- protected/modules/knowledge/views/manage/_list.php
+++ protected/modules/knowledge/views/manage/_list.php
@@ -1,20 +1,24 @@
+<?php
+$subject = LanguageHelper::changeDB($data->subject, $data->subject_en);
+$detail = LanguageHelper::changeDB($data->detail, $data->detail_en);
+?>
 <ul class="knowledgedata">
     <li>
         <div  class="knowledgeimg" style="background: url('/file/knowledge/<?php echo $data->image; ?>') center center;
               height:150px; background-size: auto 150px; border-bottom: 1px solid #ccc; background-repeat: no-repeat;   -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;">
+              -moz-background-size: cover;
+              -o-background-size: cover;
+              background-size: cover;">
             <a href="/knowledge/default/view/id/<?php echo $data->id; ?>"></a>
         </div> 
 
         <div class="arrow"></div> 
 
         <div class="textpad">
-            <h3><a href="/knowledge/default/view/id/<?php echo $data->id; ?>"><?php echo Tool::limitString($data->subject); ?></a></h3>
+            <h3><a href="/knowledge/default/view/id/<?php echo $data->id; ?>"><?php echo Tool::limitString($subject); ?></a></h3>
             <span>
                 <?php
-                echo Tool::limitString(preg_replace('/(<[^>]+) style=".*?"/i', '$1', ereg_replace('&nbsp;', ' ', $data->detail)), 150);
+                echo Tool::limitString(preg_replace('/(<[^>]+) style=".*?"/i', '$1', ereg_replace('&nbsp;', ' ', $detail)), 150);
                 ?>
             </span>
         </div>
