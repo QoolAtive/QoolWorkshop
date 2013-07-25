20f8a7190db948455711511e081797073a8810fc
annbluesky <ann.kanowan@gmail.com>
2013-07-25 17:17:35 +0700

    หน้า manage_grouplink  ใส่ Function แปลภาษา ในหน้าจัดการลิงก์หน่วยงาน

--- protected/modules/link/views/default/manage_grouplink.php
+++ protected/modules/link/views/default/manage_grouplink.php
@@ -1,12 +1,13 @@
 <h3 class="barH3">
     <span><i class="icon-link"></i><?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'กลุ่มลิงก์'); ?></span>
 </h3>
-
-<a class="linkgroupbtn fancybox.ajax btn" href="<?php echo CHtml::normalizeUrl(array("/link/default/groupform")); ?>">
-    <?php
-    echo Yii::t('language', 'เพิ่ม') . Yii::t('language', 'กลุ่มลิงก์');
-    ?>
-</a>
+<div class="btnMngED clearfix">
+    <a class="linkgroupbtn fancybox.ajax btn" href="<?php echo CHtml::normalizeUrl(array("/link/default/groupform")); ?>">
+        <?php
+        echo Yii::t('language', 'เพิ่ม') . Yii::t('language', 'กลุ่มลิงก์');
+        ?>
+    </a>
+</div>  
 <div id="gridview_group">
     <?php
     echo $this->renderPartial('_gridview_grouplink', array(
@@ -15,12 +16,14 @@
     ));
     ?>
 </div>
-<?php
-echo CHtml::button(Yii::t('language', 'กลับไปหน้าที่แล้ว'), array(
-    'onclick' => 'window.location = "' . CHtml::normalizeUrl(array("/link/default/manageLink")) . '"'));
-?>
 <div style="color: red; text-align: right;">    
     <?php
-    echo '* ' . Yii::t('language', 'หมายเหตุ') . Yii::t('language', 'บางกลุ่มไม่สามารถลบได้ เพราะยังมีสมาชิกอยู่ภายในกลุ่มลิงก์');
+    echo '*** ' . Yii::t('language', 'หมายเหตุ') .' : '. Yii::t('language', 'บางกลุ่มไม่สามารถลบได้ เพราะยังมีสมาชิกอยู่ภายในกลุ่มลิงก์');
+    ?>
+</div>
+<div class="btnForm l_btn">
+    <?php
+    echo CHtml::button(Yii::t('language', 'กลับไปหน้าที่แล้ว'), array(
+        'onclick' => 'window.location = "' . CHtml::normalizeUrl(array("/link/default/manageLink")) . '"'));
     ?>
 </div>
