ec41082d73f63c88331810699c059c897879de3f
annbluesky <ann.kanowan@gmail.com>
2013-07-25 13:48:38 +0700

    หน้า _side_bar ใส่ Function แปลภาษา ในส่วนจัดการบทความ (Knowledge) ส่วน Admin เพิ่มเรียก ข้อมูลจาก field ใน Database จาก  Helper

--- protected/modules/knowledge/views/manage/_side_bar.php
+++ protected/modules/knowledge/views/manage/_side_bar.php
@@ -1,12 +1,12 @@
 <div class="menuitem">
     <ul>
-        <li class="boxhead"><img src="/img/iconpage/knowledge.png"/></li>
+        <li class="boxhead"><img src="<?php echo Yii::t('language', '/img/iconpage/knowledge.png'); ?>"/></li>
     </ul>
     <ul class="tabs clearfix">
         <?php
         $list = array(
-            array('text' => 'เพิ่มบทความ', 'link' => '/knowledge/manage/insert'),
-            array('text' => 'บทความทั้งหมด', 'link' => '/knowledge/manage/knowledge'),
+            array('text' => Yii::t('language', 'เพิ่ม'). Yii::t('language' ,' บทความ'), 'link' => '/knowledge/manage/insert'),
+            array('text' => Yii::t('language', 'บทความทั้งหมด'), 'link' => '/knowledge/manage/knowledge'),
         );
         foreach ($list as $ls) {
             echo "<li>" . CHtml::link($ls['text'], $ls['link'], array('rel' => 'view' . ++$n)) . "</li>";
