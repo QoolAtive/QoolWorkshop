c2acf81f6e399270d1ece69601f5368b2223efea
annbluesky <ann.kanowan@gmail.com>
2013-07-25 17:04:27 +0700

    หน้า manage_grouplink  ใส่ Function แปลภาษา ในหน้าจัดการลิงก์หน่วยงาน

--- protected/modules/link/views/default/manage_grouplink.php
+++ protected/modules/link/views/default/manage_grouplink.php
@@ -1,28 +1,13 @@
-<!--<script>
-    function hideDiv(data){
-        $('#groupform').hide().html(data).fadeOut();
-    }
-    function showDiv(data){
-        $('#groupform').hide().html(data).fadeIn();
-    }
-</script>-->
-
 <h3 class="barH3">
-    <span><?php echo Yii::t('language', 'จัดการกลุ่มลิ้งก์'); ?></span>
+    <span><i class="icon-link"></i><?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'กลุ่มลิงก์'); ?></span>
 </h3>
 
-<a class="linkgroupbtn fancybox.ajax btn" href="<?php echo CHtml::normalizeUrl(array("/link/default/groupform"));?>"><?php echo Yii::t('language', 'เพิ่มชื่อกลุ่มลิ้งค์'); ?></a>
-<?php
-//echo CHtml::link('เพิ่มชื่อกลุ่มลิ้งค์', '/link/default/groupform');
-//echo CHtml::ajaxButton(
-//        'เพิ่มชื่อกลุ่มลิ้งค์', CHtml::normalizeUrl(array('/link/default/groupform')), array('update' => '#groupform', 'success' => 'showDiv')
-//);
-
-?>
-
-
+<a class="linkgroupbtn fancybox.ajax btn" href="<?php echo CHtml::normalizeUrl(array("/link/default/groupform")); ?>">
+    <?php
+    echo Yii::t('language', 'เพิ่ม') . Yii::t('language', 'กลุ่มลิงก์');
+    ?>
+</a>
 <div id="gridview_group">
-
     <?php
     echo $this->renderPartial('_gridview_grouplink', array(
         'model' => $model,
@@ -34,4 +19,8 @@
 echo CHtml::button(Yii::t('language', 'กลับไปหน้าที่แล้ว'), array(
     'onclick' => 'window.location = "' . CHtml::normalizeUrl(array("/link/default/manageLink")) . '"'));
 ?>
-<div style="color: red; text-align: right;">*หมายเหตุ บางกลุ่มไม่สามารถลบได้ เพราะยังมีสมาชิกอยู่ภายในกลุ่มลิ้งก์</div>
+<div style="color: red; text-align: right;">    
+    <?php
+    echo '* ' . Yii::t('language', 'หมายเหตุ') . Yii::t('language', 'บางกลุ่มไม่สามารถลบได้ เพราะยังมีสมาชิกอยู่ภายในกลุ่มลิงก์');
+    ?>
+</div>
