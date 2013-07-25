b027bd2a2c6df34e03808699614b92506e4c8a8c
annbluesky <ann.kanowan@gmail.com>
2013-07-25 16:21:51 +0700

    หน้า index ใส่ Function แปลภาษา ในหน้าจัดการลิงก์หน่วยงาน

--- protected/modules/link/views/default/index.php
+++ protected/modules/link/views/default/index.php
@@ -1,8 +1,3 @@
-<?php
-$currentLang = Yii::app()->language;
-//echo "<br />currentLang: " . $currentLang . "<br />";
-?>
-
 <div class="sidebar">
     <div class="menuitem">
         <ul>
@@ -22,62 +17,56 @@ $currentLang = Yii::app()->language;
 
 <div class="linksearch">
     <div class="_100">
-    <h3><?php echo Yii::t('language', 'ค้นหา'); ?> : </h3>
-</div>
-    <div class="_50">
-    <?php
-    $form = $this->beginWidget('CActiveForm', array(
-        'id' => 'search-form',
-    ));
-    echo CHtml::textField('name', '', array('placeholder' => Yii::t('language', 'ค้นหาตามชื่อลิงก์')));
-
-    $feild_name = "name_th";
-    if ($currentLang == 'en') { // Change Language TH/EN Select Data for show in Drop Down List
-            $feild_name = "name_en";
-    }
-
-    ?>
-</div>
-    <div class="_50">
-
-    <?php
-//    echo "<br />feild_name: " . $feild_name . "<br />";
-
-    echo CHtml::DropDownList(
-            'group_id', $group, array('0' => " - ".Yii::t('language', 'ค้นหาตามกลุ่มลิงก์')." - ") + CHtml::listData(LinkGroup::model()->findAll(), "id", $feild_name)
-    );
-    ?>
+        <h3><?php echo Yii::t('language', 'ค้นหา'); ?> : </h3>
     </div>
-     <div class="_100 textcenter">
-    <?php
-    echo CHtml::submitButton(Yii::t('language', 'ค้นหา'));
-    $this->endWidget();
-    ?>
+    <div class="_50">
+        <?php
+        $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'search-form',
+        ));
+        echo CHtml::textField('name', '', array('placeholder' => Yii::t('language', 'ค้นหาตามชื่อลิงก์')));
+        ?>
+    </div>
+    <div class="_50">
+
+<?php
+//    echo "<br />feild_name: " . $feild_name . "<br />";
+// Change Language TH/EN Select Data for show in Drop Down List
+$feild_name = LanguageHelper::changeDB('name_th', 'name_en');
+echo CHtml::DropDownList(
+        'group_id', $group, array('0' => " - " . Yii::t('language', 'ค้นหาตามกลุ่มลิงก์') . " - ") + CHtml::listData(LinkGroup::model()->findAll(), "id", $feild_name)
+);
+?>
+    </div>
+    <div class="_100 textcenter">
+<?php
+echo CHtml::submitButton(Yii::t('language', 'ค้นหา'));
+$this->endWidget();
+?>
 
         <?php
-    if (Yii::app()->user->isAdmin()) {
-        echo CHtml::button(Yii::t('language', 'จัดการลิงก์'), array(
-            'onclick' => 'window.location="/link/default/managelink"'
-        ));
-    }
-    ?>
-</div>
+        if (Yii::app()->user->isAdmin()) {
+            echo CHtml::button(Yii::t('language', 'จัดการลิงก์'), array(
+                'onclick' => 'window.location="/link/default/managelink"'
+            ));
+        }
+        ?>
+    </div>
 
 </div>
 <ul class="linklist">
-    <?php
-    if (empty($list)) {
-        echo Yii::t('language', "ไม่พบลิงก์ที่เกี่ยวข้อง");
-    } else {
-        foreach ($list as $l) {
-            $name = $l['name_th'];
-            if ($currentLang == 'en') {
-                if ($l['name_en'] != '') {
-                    $name = $l['name_en'];
-                }
+<?php
+if (empty($list)) {
+    echo Yii::t('language', "ไม่พบลิงก์ที่เกี่ยวข้อง");
+} else {
+    foreach ($list as $l) {
+        $name = $l['name_th'];
+        if ($currentLang == 'en') {
+            if ($l['name_en'] != '') {
+                $name = $l['name_en'];
             }
-            ?>
-
+        }
+        ?>
             <li>
                 <ul class="innerlogo">
                     <li><a href="<?php echo $l['link']; ?>" target="_blank"><img src="<?php echo $l['img_path']; ?>"></a> </li>
@@ -85,10 +74,9 @@ $currentLang = Yii::app()->language;
                     <li><a href="<?php echo $l['link']; ?>" target="_blank"><?php echo $name; ?></a></li>
                 </ul>
             </li>
-
-            <?php
-        }
+        <?php
     }
-    ?>
+}
+?>
 </ul>
 
