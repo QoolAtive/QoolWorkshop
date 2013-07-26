8b9be63333bfdc9a25f4d0a4a28911073786dea1
iParin <suphasit@gmail.com>
2013-07-26 17:32:20 +0700

    Merge branch 'BB2'
    Conflicts:
    	test1.php
    	test3.php

--- test1.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "test3B23256565656";
-/*sssss
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
--- test3.php
+++ test3.php
@@ -1,5 +1,9 @@
 <?php
+<<<<<<< HEAD
 echo "test3group-4-2";
+=======
+echo "test3group-5";
+>>>>>>> BB2
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
