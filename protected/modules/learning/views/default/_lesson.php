b889f00763cd6789859f6027375c8fee36fe8953
Vasit <Vasit@192.168.1.148>
2013-07-29 18:03:46 +0700

    learning_แก้ไข หน้า lesson โชว์ข้อมูลผิด

--- protected/modules/learning/views/default/_lesson.php
+++ protected/modules/learning/views/default/_lesson.php
@@ -17,68 +17,53 @@ $this->renderPartial('_side_bar', array(
 ?>
 <div class="content">
     <div class="tabcontents">
-        <div id="view1" class="tabcontent">
-            <?php
-            $this->widget('zii.widgets.CListView', array(
-                'dataProvider' => $learningGroup->getData(),
-                'itemView' => '_clist_learning_group',
-                'summaryText' => false,
-//                'sortableAttributes' => array(
-//                    'title',
-//                    'create_time' => 'Post Time',
-//                ),
-            ));
-            ?>
+        <?php
+        if (Yii::app()->user->isAdmin()) {
+            echo "<div style='float: right;'>";
+            echo CHtml::button('แก้ไข', array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+                    '/learning/manage/insertLearning/id/' . $model->id
+                )) . "'")
+            );
+            echo "</div>";
+        }
+        ?>
+
+        <h2 class="learninghead"><img src="/img/book.png"/>
+            <?php echo $model->subject; ?>
+        </h2>
+        <img class="demoshadowtop" src="/img/shadow.png">
+        <iframe width="745" height="415" src="<?php echo $modelVideo->video; ?>" frameborder="0" allowfullscreen></iframe>
+        <img class="demoshadowbuttom" src="/img/shadow.png">
+        <!--<div class="_100">-->
+        <?php echo $model->detail; ?>
+        <!--</div>-->
+        <div class="clearfix"></div>
+
+        <?php if (isset($modelFile)) echo CHtml::link(CHtml::image('/img/download.png', '', array('class' => 'downloadbtn')), array('/learning/default/readingPdf/', 'id' => $modelFile->id)); ?>
+        <hr class="demohr"> 
+        <?php if (!empty($lessonNext)) { ?>
+
+
+            <ul class="nextlearn">
+                <li>
+                    <iframe width="210px" src="<?php echo $lessonNextVideo->video; ?>" frameborder="0" allowfullscreen></iframe>
+
+                </li>
+
+                <li>
+                    <?php echo CHtml::link($lessonNext->subject, array('/learning/default/lesson', 'id' => $lessonNext->id), array('style' => 'font-size: 14px;')); ?>
+                    <?php echo Tool::limitString(preg_replace('/(<[^>]+) style=".*?"/i', '$1', ereg_replace('&nbsp;', ' ', $lessonNext->detail)), 350); ?>
+                </li>
+            </ul>
+
+        <?php } ?>
+        <div class="clearfix">
+            <div class="_100"></div>
         </div>
-        <div id="view2" class="tabcontent ">
+        <div style="text-align: center;padding: 10px;">
             <?php
-            if (Yii::app()->user->isAdmin()) {
-                echo "<div style='float: right;'>";
-                echo CHtml::button('แก้ไข', array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
-                        '/learning/manage/insertLearning/id/' . $model->id
-                    )) . "'")
-                );
-                echo "</div>";
-            }
+            echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array('onClick' => "history.go(-1)"));
             ?>
-
-            <h2 class="learninghead"><img src="/img/book.png"/>
-                <?php echo $model->subject; ?>
-            </h2>
-            <img class="demoshadowtop" src="/img/shadow.png">
-            <iframe width="745" height="415" src="<?php echo $modelVideo->video; ?>" frameborder="0" allowfullscreen></iframe>
-            <img class="demoshadowbuttom" src="/img/shadow.png">
-            <!--<div class="_100">-->
-            <?php echo $model->detail; ?>
-            <!--</div>-->
-            <div class="clearfix"></div>
-
-            <?php if (isset($modelFile)) echo CHtml::link(CHtml::image('/img/download.png', '', array('class' => 'downloadbtn')), array('/learning/default/readingPdf/', 'id' => $modelFile->id)); ?>
-            <hr class="demohr"> 
-            <?php if (!empty($lessonNext)) { ?>
-
-
-                <ul class="nextlearn">
-                    <li>
-                        <iframe width="210px" src="<?php echo $lessonNextVideo->video; ?>" frameborder="0" allowfullscreen></iframe>
-
-                    </li>
-
-                    <li>
-                        <?php echo CHtml::link($lessonNext->subject, array('/learning/default/lesson', 'id' => $lessonNext->id), array('style' => 'font-size: 14px;')); ?>
-                        <?php echo Tool::limitString(preg_replace('/(<[^>]+) style=".*?"/i', '$1', ereg_replace('&nbsp;', ' ', $lessonNext->detail)), 350); ?>
-                    </li>
-                </ul>
-
-            <?php } ?>
-            <div class="clearfix">
-                <div class="_100"></div>
-            </div>
-            <div style="text-align: center;padding: 10px;">
-                <?php
-                echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array('onClick' => "history.go(-1)"));
-                ?>
-            </div>
         </div>
     </div>
 </div>
