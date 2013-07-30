b3473779f7a081a6ab8484025a2dbd3b6a5dbdcd
annbluesky <ann.kanowan@gmail.com>
2013-07-30 11:47:24 +0700

    ใส่ Function แปลภาษา ในส่วน footer layout

--- protected/views/layouts/_index-footer.php
+++ protected/views/layouts/_index-footer.php
@@ -1,8 +1,29 @@
 <div class="page">
     <div class="foottext">
-        <p>สำนักพาณิชย์อิเล็กทรอนิกส์ กรมพัฒนาธุรกิจการค้า กระทรวงพาณิชย์</p>
-        <p>44/100 ถนน นนทบุรี 1 ตำบลบางกระสออำเภอเมือง จังหวัดนนทบุรี 11000 </p>
-        <p>โทร. 0-2528-7600 ต่อ 3191,3192 โทรสาร. 0-2547-5973 สายด่วน 1570</p>
+        <p>
+            <?php
+            echo Yii::t('language', 'สำนักพาณิชย์อิเล็กทรอนิกส์');
+            echo ' ' . Yii::t('language', 'กรมพัฒนาธุรกิจการค้า');
+            echo ' ' . Yii::t('language', 'กระทรวงพาณิชย์');
+            ?>  
+        </p>
+        <p>                
+            <?php
+            echo '44/100';
+            echo Yii::t('language', 'ถนนนนทบุรี 1');
+            echo ' ' . Yii::t('language', 'ตำบลบางกระสอ');
+            echo ' ' . Yii::t('language', 'อำเภอเมือง');
+            echo ' ' . Yii::t('language', 'จังหวัดนนทบุรี');
+            echo ' 11000';
+            ?>
+        </p>
+        <p>
+            <?php
+            echo Yii::t('language', 'โทร. 0-2528-7600 ต่อ 3191,3192');
+            echo ' ' . Yii::t('language', 'โทรสาร. 0-2547-5973');
+            echo ' ' . Yii::t('language', 'สายด่วน 1570');
+            ?>
+        </p>
     </div>
     <div class="footright">
         <img alt="facebook" src="/img/face.png"/>
