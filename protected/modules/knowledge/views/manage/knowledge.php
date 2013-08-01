42aa2b0c703e7eeeb8c2fcd100cdebd2c0a5d486
annbluesky <ann.kanowan@gmail.com>
2013-08-01 16:39:07 +0700

    Knowledge: หน้า _knowledge ในส่วนจัดการบทความ  ใส่ Function แปลภาษา + จัดปุ่ม ใส่ Class + hr + icon + link menu
    Knowledge: หน้า _knowledge ในส่วนจัดการบทความ
    + ใส่ Function แปลภาษา
    + จัดปุ่ม ใส่ Class
    + hr
    + icon
    + link menu

--- protected/modules/knowledge/views/manage/knowledge.php
+++ protected/modules/knowledge/views/manage/knowledge.php
@@ -4,17 +4,33 @@
             <li class="boxhead"><img src="<?php echo Yii::t('language', '/img/iconpage/knowledge.png'); ?>"/></li>
         </ul>
         <ul class="tabs clearfix">
-            <li><a href="/knowledge/default/index"><?php echo Yii::t('language', 'บทความ'); ?></a></li>
-            <li><a href="/learning/default/home"><?php echo Yii::t('language', 'การเรียนรู้'); ?></a></li>
-            <li ><a href="/knowledge/manage/knowledge"><?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'บทความ'); ?></a></li>
-            <li><a href="/learning/manage/learning"><?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'การเรียนรู้'); ?></a></li>
+            <li>
+                <a href="/knowledge/default/index" rel="view-1">
+                    <?php echo Yii::t('language', 'บทความ'); ?>
+                </a>
+            </li>
+            <li>
+                <a href="/learning/default/home"  rel="view-2">
+                    <?php echo Yii::t('language', 'การเรียนรู้'); ?>
+                </a>
+            </li>
+            <li class="selected">
+                <a href="/knowledge/manage/knowledge" rel="manage-1">
+                    <?php echo Yii::t('language', 'จัดการ') . '<br />' . Yii::t('language', 'บทความ'); ?>
+                </a>
+            </li>
+            <li>
+                <a href="/learning/manage/learning" rel="manage-2">
+                    <?php echo Yii::t('language', 'จัดการ') . '<br />' . Yii::t('language', 'การเรียนรู้'); ?>
+                </a>
+            </li>
 
             <?php
-            $list = array(
-                array('text' => Yii::t('language', 'บทความทั้งหมด'), 'link' => '/knowledge/manage/knowledge', 'select' => 'selected'),
-//                array('text' => Yii::t('language', 'บทความทั้งหมด'), 'link' => '/knowledge/manage/knowledgeAll', 'select' => ''),
-            );
-            echo Tool::GenList($list);
+//            $list = array(
+//                array('text' => Yii::t('language', 'บทความทั้งหมด'), 'link' => '/knowledge/manage/knowledge', 'select' => 'selected'),
+////                array('text' => Yii::t('language', 'บทความทั้งหมด'), 'link' => '/knowledge/manage/knowledgeAll', 'select' => ''),
+//            );
+//            echo Tool::GenList($list);
             ?> 
 
         </ul>
@@ -22,9 +38,19 @@
 </div>
 <div class="content">
     <div class="tabcontents">
-        <h3><?php echo Yii::t('language', 'บทความทั้งหมด'); ?></h3>
-        <hr>
-        <div style="text-align: center;">
+        <h3 class="barH3">
+            <span>
+                <i class='icon-lightbulb'></i>                
+                <a href="<?php echo CHtml::normalizeUrl(array("/knowledge/default/knowledge")); ?>">
+                    <?php echo Yii::t('language', 'บทความทั้งหมด'); ?>
+                </a>
+                <i class="icon-chevron-right"></i>
+                <?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'บทความ'); ?>
+            </span>
+        </h3>
+
+        <div class="txt-cen">
+            <hr>
             <?php
             echo CHtml::button(Yii::t('language', 'เพิ่ม') . Yii::t('language', 'บทความ'), array(
                 'onClick' => "window.location='" . CHtml::normalizeUrl(array(
@@ -32,8 +58,9 @@
                 )) . "'")
             );
             ?>
+            <hr>
         </div>
-        <hr>
+
         <?php
         $this->renderPartial('_grid_all_knowledge', array(
             'model' => $model,
