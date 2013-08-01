a88ea442cff25eb5850f2fb761acdd3dea1feea2
Pui Wongho <yeosa4@gmail.com>
2013-08-01 15:39:54 +0700

    commit_test3

--- test3.php
+++ test3.php
@@ -10,25 +10,13 @@
 <div class="content">
 <?php
 
-echo "test3";
+echo "EEEEEEEEEEEEEEE";
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- * 
- * 
- * 9656565
- * 
- * 
- * 
- * 
- */
+echo "ZZZZZZZZZZZZZZZz";
 
-echo "B test4";
+echo "XXXXXXXXXXXXXx";
 
-echo "B test5";
-
-echo "B.ss.2.1";
+echo "AAAAAAAAAAA";
 
 
 if(true){
