1aecf8e5fe730691c466ca4df8748ea35bec07d4
Piyanan <pyn@live.com>
2013-08-01 17:26:34 +0700

    เพิ่มหน้า select themes

---
+++ protected/modules/webSimulation/controllers/ManageShopController.php
@@ -0,0 +1,25 @@
+<?php
+
+class ManageShopController extends Controller {
+
+    public function actionIndex() {
+        $model = new WebShop();
+        if (isset($_POST['WebShop'])) {
+            $model->attributes = $_POST['WebShop'];
+            $model->mem_user_id = Yii::app()->user->id;
+            $model->creat_at = date("Y-m-d H:i:s");
+            if ($model->save()) {
+                echo "<script language='javascript'>
+                        alert('" . Yii::t('language', 'บันทึก') . Yii::t('language', 'ข้อมูล') . Yii::t('language', 'เรียบร้อย') . "');
+                        window.top.location.href = '" . CHtml::normalizeUrl(array('/webSimulation/manageShop/selectThemes')) . "';
+                  </script>";
+            }
+        }
+        $this->render('index', array('model' => $model));
+    }
+    
+    public function actionSelectThemes(){
+        $this->render('select_themes', array('model' => $model));
+    }
+
+}
