b383da4d3b0c2b18b65373e526046b760b2c4012
Pui Wongho <yeosa4@gmail.com>
2013-08-01 15:39:04 +0700

    commit_test2

--- test2.php
+++ test2.php
@@ -1,3 +1,10 @@
+<style type="text/css">
+    p{
+        font-weight: bold;
+        font-size: 2em;
+        color:pink;
+    }
+</style>
 <?php
 
 echo "test2";
@@ -5,9 +12,9 @@ echo "test2";
 echo "yo";
 
 echo "Tottenham 5-0 Arsenal";
-
+echo "<p>";
 echo "ช้าง ช้าง ช้าง....";
-
+echo "</p>";
 echo "ตับ ตับ ตับ";
 
 ?>
