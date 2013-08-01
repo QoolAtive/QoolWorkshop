bd41baeadeb12c7254fc9b6bc7b300cc538be58b
Piyanan <pyn@live.com>
2013-08-01 10:27:23 +0700

    test3

--- test3.php
+++ test3.php
@@ -2,16 +2,5 @@
 
 echo "test3";
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- * 
- * 
- * 9656565
- * 
- * 
- * 
- * 
- */
 echo "ann33";
 ?>
