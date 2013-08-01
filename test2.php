da9a2cc6cafa01a8ebaf08447f9052108061d378
Piyanan <pyn@live.com>
2013-08-01 10:27:15 +0700

    test2

--- test2.php
+++ test2.php
@@ -2,16 +2,5 @@
 
 echo "test2";
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- * 
- * 
- * 
- * 3b3b3b3b3b3b3b3b3b
- * prepare     
- * 
- * 
- */
 echo "ann2";
 ?>
