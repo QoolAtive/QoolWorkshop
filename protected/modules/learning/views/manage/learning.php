8258b2788ae8984d941e5c477481d5a0e9b737dc
annbluesky <ann.kanowan@gmail.com>
2013-08-05 15:01:34 +0700

    Learning: ส่วน Admin หน้า Learning

--- protected/modules/learning/views/manage/learning.php
+++ protected/modules/learning/views/manage/learning.php
@@ -1,18 +1,31 @@
 <?php
 $this->renderPartial('_side_bar', array(
-    'select2' => 'selected',
-    'select1' => '',
+    'select_list' => 2,
 ));
 ?>
 <div class="content">
     <div class="tabcontents">
         <div id="view2" class="tabcontent">
             <div>
-                <h3><?php echo Yii::t('language', 'กลุ่มบทเรียน'); ?></h3>
+                <h3 class="headfont">
+                    <span>
+                        <i class="icon-bookmark-empty"></i> 
+                        <a href="<?php echo CHtml::normalizeUrl(array("/learning/default/home")); ?>">
+                            <?php echo Yii::t('language', 'การเรียนรู้'); ?>
+                        </a>
+                        <i class="icon-chevron-right"></i>
+                        <a href="/learning/manage/learning">
+                            <?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'การเรียนรู้'); ?>
+                        </a>
+                        <i class="icon-chevron-right"></i>
+                        <?php echo Yii::t('language', 'บทเรียน'); ?>
+                    </span>
+                </h3>
                 <hr>
                 <div style="text-align: center;">
                     <?php
-                    echo CHtml::button(Yii::t('language', 'เพิ่มบทเรียน'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+                    echo CHtml::button(Yii::t('language', 'เพิ่ม').Yii::t('language', 'บทเรียน'), array(
+                        'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                             '/learning/manage/InsertLearning'
                         )) . "'")
                     );
