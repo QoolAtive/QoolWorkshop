b1f19c79cd55e4745eff576ff32f8fe302214cda
golfiti <golfitiz@gmail.com>
2013-08-07 12:14:29 +0700

    fix fancy box ไม่ขึ้น

--- js/config.js
+++ js/config.js
@@ -1,22 +1,4 @@
 
-$(document).ready(function($){
-    $('#featured').orbit({
-     animation: 'horizontal-slide',                  // fade, horizontal-slide, vertical-slide, horizontal-push
-     animationSpeed: 800,                // how fast animtions are
-     timer: true,            // true or false to have the timer
-     advanceSpeed: 4000,         // if timer is enabled, time between transitions 
-     pauseOnHover: false,        // if you hover pauses the slider
-     startClockOnMouseOut: false,    // if clock should start on MouseOut
-     startClockOnMouseOutAfter: 1000,    // how long after MouseOut should the timer start again
-     directionalNav: false,       // manual advancing directional navs
-     captions: true,             // do you want captions?
-     captionAnimation: 'slideOpen',       // fade, slideOpen, none
-     captionAnimationSpeed: 800,     // if so how quickly should they animate in
-     bullets: true,             // true or false to activate the bullet navigation
-  
-});
-        });
-
 
 $(document).ready(function($){
         // Get current url
@@ -249,6 +231,20 @@ $(document).ready(function() {
 
 
 
+$(document).ready(function($){
+    $('#featured').orbit({
+     bullets: true,
+     animation: 'horizontal-slide',                  // fade, horizontal-slide, vertical-slide, horizontal-push
+     animationSpeed: 800,                // how fast animtions are
+     timer: true,            // true or false to have the timer
+     advanceSpeed: 4000,         // if timer is enabled, time between transitions 
+     pauseOnHover: false,        // if you hover pauses the slider
+     startClockOnMouseOut: false,    // if clock should start on MouseOut
+     startClockOnMouseOutAfter: 1000,    // how long after MouseOut should the timer start again
+     directionalNav: false,       // manual advancing directional navs
+     captions: true             // do you want captions?
+  });
+        });
 
 
 
