36e8a3f4e1f8e7d7e7d15d219d4fab7a8f0f0d01
annbluesky <ann.kanowan@gmail.com>
2013-08-07 15:35:57 +0700

    Service Providers: ส่วน Admin หน้าแสดงรายการสินค้าและบริการ

--- protected/modules/serviceProvider/views/manage/product.php
+++ protected/modules/serviceProvider/views/manage/product.php
@@ -5,33 +5,57 @@ $this->renderPartial('_side_bar', array(
 ));
 
 $title = SpCompany::model()->find('id=:id', array(':id' => $id));
+$company_name = LanguageHelper::changeDB($title->name, $title->name_en);
 ?>
 <div class="content">
     <div class="tabcontents">
-        <h3><img src="/img/iconform.png"><?php echo Yii::t('language', 'จัดการสินค้า') . ' ' . Yii::t('language', 'บริษัท') . ' ' . $title->name; ?></h3>
-        <hr>
+        <h3 class="barH3">
+            <span>
+                <i class="icon-compass"></i> 
+                <a href="<?php echo CHtml::normalizeUrl(array("/serviceProvider/default/index")); ?>">
+                    <?php echo Yii::t('language', 'ผู้ให้บริการทั้งหมด'); ?>
+                </a>
+                <i class="icon-chevron-right"></i>
+                <a href="<?php echo CHtml::normalizeUrl(array("/serviceProvider/manage/typeBusiness")); ?>">
+                    <?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'บริการ'); ?>
+                </a> 
+                <i class="icon-chevron-right"></i>
+                <a href="<?php echo CHtml::normalizeUrl(array("/serviceProvider/manage/company")); ?>">
+                    <?php echo Yii::t('language', 'พาร์ทเนอร์'); ?>
+                </a>
+                <i class="icon-chevron-right"></i>
+                <?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'สินค้าและบริการ') . ' (' . $company_name . ') '; ?>
+            </span>
+        </h3>
+
         <div style="text-align: center;">
+            <hr>
             <?php
-            echo CHtml::button(Yii::t('language', 'เพิ่มสินค้า'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+            echo CHtml::button(Yii::t('language', 'เพิ่ม') . Yii::t('language', 'สินค้าและบริการ'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/serviceProvider/manage/insertProduct/id/' . $id
                 )) . "'")
             );
             ?>
             <hr>
-            <?php
-            $this->renderPartial('_grid_product', array(
-                'model' => $model,
-                'id' => $id,
-            ));
-            ?>
+            <div>
+                <?php
+                $this->renderPartial('_grid_product', array(
+                    'model' => $model,
+                    'id' => $id,
+                ));
+                ?>
+            </div>
         </div>
         <div class="_100 textcenter">
+            <hr>
             <?php
             echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/serviceProvider/manage/company'
                 )) . "'")
             );
             ?>
+            <hr>
         </div>
     </div>
 </div>
