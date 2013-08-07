8858cbdbbc8102f5e3dafc34fcd78a946726ba58
vasitjuntong <N_vasit@hotmail.com>
2013-08-07 10:16:20 +0700

    sync2

--- protected/modules/serviceProvider/views/default/_detail.php
+++ protected/modules/serviceProvider/views/default/_detail.php
@@ -47,15 +47,14 @@
 </div>
 <div class="content">
     <div class="tabcontents" >
-<<<<<<< HEAD
         <div class="clearfix" style="border: 1px solid #e0e0e0; display: inline-block; width: 100%;">
 
             <div id="featured" > 
-                    <img src="/file/banner/default.jpg" style="height: 220px;" />
-                    <img src="/file/banner/default.jpg" style="height: 220px;"/>
-                    <img src="/file/banner/default.jpg" style="height: 220px;"/>
+                <img src="/file/banner/default.jpg" style="height: 220px;" />
+                <img src="/file/banner/default.jpg" style="height: 220px;"/>
+                <img src="/file/banner/default.jpg" style="height: 220px;"/>
             </div>
-                <img src="/file/logo/default.jpg" style="float: right;" width="220">
+            <img src="/file/logo/default.jpg" style="float: right;" width="220">
 
         </div>
         <div class="clearfix" style="border: 1px #c9c9c9 solid;padding: 5px 15px;">
@@ -223,177 +222,6 @@
             }
             ?>
             <hr>
-=======
-        <div style="border: 1px solid #e0e0e0; display: inline-block; width: 100%;">
-
-            <div style="border: 1px solid #e0e0e0; display: inline-block; width: 100%;">
-
-                <div class="clearfix" style="border: 1px #c9c9c9 solid;padding: 5px 15px;">
-                    <h2>
-                        <img src="/img/icontopic.png" />
-                        <?php
-                        echo Yii::t('language', 'ข้อมูลของบริษัท');
-                        if (Yii::app()->user->isAdmin()) {
-                            echo CHtml::button(
-                                    Yii::t('language', 'แก้ไข'), array(
-                                'class' => "grey", // btnedit grey
-                                'style' => 'margin-left: 656px; margin-top: 5px; position:absolute;',
-                                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
-                                    '/serviceProvider/manage/insertCompany/id/' . $model->id
-                                )) . "'")
-                            );
-                        }
-                        ?>
-                    </h2>
-                    <table>
-                        <tr>
-                            <td><?php echo Yii::t('language', 'ชื่อบริษัท'); ?></td>
-                            <td> : </td>
-                            <td>
-                                <?php
-                                $name = LanguageHelper::changeDB($model->name, $model->name_en);
-                                echo $name;
-                                ?>
-                            </td>
-                        </tr>
-
-                        <tr>
-                            <td><?php echo Yii::t('language', 'ประเภทผู้ให้บริการ'); ?></td>
-                            <td> : </td>
-                            <td>
-                                <?php
-                                $type = SpTypeCom::model()->findAll('com_id=:com_id', array(':com_id' => $model->id));
-                                foreach ($type as $data) {
-                                    $type_name = SpTypeBusiness::model()->find('id=:id', array(':id' => $data['type_id']));
-                                    $type_name = LanguageHelper::changeDB($type_name->name, $type_name->name_en);
-                                    $data_type .= $type_name . ', ';
-                                }
-                                echo rtrim($data_type, ', ');
-                                ?>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><?php echo Yii::t('language', 'ที่ตั้ง'); ?></td>
-                            <td> : </td>
-                            <td>
-                                <?php
-                                $address = LanguageHelper::changeDB($type_name->address, $type_name->address_en);
-                                echo $address;
-                                ?>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td><?php echo Yii::t('language', 'เว็บไซต์'); ?></td>
-                            <td> : </td>
-                            <td><?php echo $model->website; ?></td>
-                        </tr>
-                        <?php if ($model->brochure) { ?>
-                            <tr>
-                                <td><?php echo Yii::t('language', 'โบรชัวร์'); ?></td>
-                                <td> : </td>
-                                <td>
-                                    <?php
-                                    echo CHtml::link('ดาวน์โหลด', array(
-                                        '/serviceProvider/default/readingPdf/', 'id' => $model->id), array(
-                                        'target' => '_bank'
-                                    ));
-                                    ?>
-                                </td>
-                            </tr>
-                        <?php } ?>
-
-                    </table>
-                    <?php
-                    $infor = LanguageHelper::changeDB($model->infor, $model->infor_en);
-                    echo $infor;
-                    ?>
-                </div>
-                <?php
-                $dp_product_best_sell = new CActiveDataProvider('SpProduct', array(
-                    'criteria' => array(
-                        'condition' => 'guide = 1 and main_id = ' . $model->id,
-                        'order' => 'id desc',
-                    ),
-                    'pagination' => array(
-                        'pageSize' => 5,
-                    ),
-                ));
-                if ($dp_product_best_sell->itemCount > 0) {
-                    ?>
-                    <h2><img src="/img/icontopic.png" /><?php echo Yii::t('language', 'สินค้าขายดี'); ?></h2>
-                    <div class="clearfix">
-                        <?php
-                        $this->widget('zii.widgets.CListView', array(
-                            'dataProvider' => $dp_product_best_sell,
-                            'itemView' => 'list_product',
-                            'summaryText' => false,
-                        ));
-                        ?>
-                    </div>
-                    <?php
-                }
-                $dp_product_promo = new CActiveDataProvider('SpProduct', array(
-                    'criteria' => array(
-                        'condition' => 'guide = 2 and main_id = ' . $model->id,
-                        'order' => 'id desc',
-                    ),
-                    'pagination' => array(
-                        'pageSize' => 5,
-                    ),
-                ));
-                if ($dp_product_promo->itemCount > 0) {
-                    ?>
-                    <h2><img src="/img/icontopic.png" /><?php echo Yii::t('language', 'โปรโมชั่น'); ?></h2>
-                    <div class="clearfix">
-                        <?php
-                        $this->widget('zii.widgets.CListView', array(
-                            'dataProvider' => $dp_product_promo,
-                            'itemView' => 'list_product',
-                            'summaryText' => false,
-                        ));
-                        ?>
-                    </div>
-                    <?php
-                }
-                $dp_product_new = new CActiveDataProvider('SpProduct', array(
-                    'criteria' => array(
-                        'condition' => 'main_id = ' . $model->id,
-                        'order' => 'id desc',
-                    ),
-                    'pagination' => array(
-                        'pageSize' => 5,
-                    ),
-                ));
-
-                if ($dp_product_new->itemCount > 0) {
-                    ?>
-                    <h2><img src="/img/icontopic.png" /><?php echo Yii::t('language', 'สินค้าใหม่'); ?></h2>
-                    <div class="clearfix">
-                        <?php
-                        $this->widget('zii.widgets.CListView', array(
-                            'dataProvider' => $dp_product_new,
-                            'itemView' => 'list_product',
-                            'summaryText' => false,
-                        ));
-                        ?>
-                    </div>
-                <?php } ?>
-                <div class="textcenter">
-                    <hr>
-                    <?php
-                    echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
-                            '/serviceProvider/default/index/id/' . $type_business_back
-                        )) . "'")
-                    );
-                    if (Yii::app()->user->isAdmin()) {
-                        echo CHtml::button(Yii::t('language', 'ย้อนกลับจัดการพาร์ทเนอร์'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
-                                '/serviceProvider/manage/company'
-                            )) . "'")
-                        );
-                    }
-                    ?>
-                    <hr>
-                </div>
-            </div>
->>>>>>> 9ad4cf5f6f52000f3556b497f997092fbebfb00e
         </div>
+    </div>
+</div>
--- protected/modules/serviceProvider/views/default/partner_group.php
+++ protected/modules/serviceProvider/views/default/partner_group.php
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-<?php
-$this->renderPartial('_side_bar', array(
-    'id' => $model->id,
-));
-?>
-<div class="content">
-    <div class="tabcontents">
-        <div class="clearfix" style="border: 1px #c9c9c9 solid;padding: 15px;">
-            <h3>
-                <img src="/img/iconform.png"> <?php echo $model->name; ?>
-            </h3>
-            <!--<div class="knowledgeview">-->
-                <?php
-                if (Yii::app()->user->isAdmin()) {
-                    echo CHtml::button(
-                            Yii::t('language', 'แก้ไข'), array(
-                        'class' => "grey", // btnedit grey
-                        'style' => 'margin-left: 650px; margin-top: 8px; position:absolute;',
-                        'onClick' => "window.location='" . CHtml::normalizeUrl(array(
-                            '/serviceProvider/manage/insertTypeBusiness/id/' . $model->id
-                        )) . "'")
-                    );
-                }
-                ?>
-
-                <div class="clearfix">
-                    <?php echo $model->about; ?>    
-                </div>
-            <!--</div>-->
-        </div>
-        <div class="clearfix" style="border: 1px #c9c9c9 solid;padding: 15px;margin-top: 5px;">
-            <h3><img src="/img/iconform.png"> Partner</h3>
-            <?php
-            $criteria = new CDbCriteria;
-            $criteria->order = 'id desc';
-            $criteria->select = "t.*, stc.type_id as type_id";
-            $criteria->condition = "stc.type_id = '" . $model->id . "'";
-            $criteria->join = "
-            left join sp_type_com stc on t.id = stc.com_id
-            ";
-
-            $partner = new CActiveDataProvider('SpCompany', array(
-                'criteria' => $criteria,
-            ));
-            $this->widget('zii.widgets.CListView', array(
-                'dataProvider' => $partner,
-                'itemView' => '_list',
-                'summaryText' => false,
-            ));
-            ?>
-        </div>
-    </div>
-</div>
-=======
 <?php
 $this->renderPartial('_side_bar', array(
     'id' => $model->id,
@@ -117,4 +62,3 @@ $about = LanguageHelper::changeDB($model->about, $model->about_en);
         </div>
     </div>
 </div>
->>>>>>> 9ad4cf5f6f52000f3556b497f997092fbebfb00e
