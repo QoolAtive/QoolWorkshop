e3df38e330622975565378cf97a1154dd9e47947
Piyanan <pyn@live.com>
2013-08-07 14:07:18 +0700

    เพิ่มfield ในตาราง web_shop_format

--- protected/models/WebShopFormatBase.php
+++ protected/models/WebShopFormatBase.php
@@ -9,6 +9,12 @@
  * @property string $logo
  * @property string $theme
  * @property string $background
+ * @property string $char_color
+ * @property string $char_size
+ * @property string $topic_color
+ * @property string $topic_size
+ * @property string $link_color
+ * @property string $link_size
  *
  * The followings are the available model relations:
  * @property WebShop $webShop
@@ -44,9 +50,11 @@ class WebShopFormatBase extends CActiveRecord
 			array('web_shop_id, theme', 'required'),
 			array('web_shop_id', 'numerical', 'integerOnly'=>true),
 			array('logo, theme, background', 'length', 'max'=>100),
+			array('char_color, topic_color, link_color', 'length', 'max'=>6),
+			array('char_size, topic_size, link_size', 'length', 'max'=>10),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('web_shop_format_id, web_shop_id, logo, theme, background', 'safe', 'on'=>'search'),
+			array('web_shop_format_id, web_shop_id, logo, theme, background, char_color, char_size, topic_color, topic_size, link_color, link_size', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -73,6 +81,12 @@ class WebShopFormatBase extends CActiveRecord
 			'logo' => 'Logo',
 			'theme' => 'Theme',
 			'background' => 'Background',
+			'char_color' => 'Char Color',
+			'char_size' => 'Char Size',
+			'topic_color' => 'Topic Color',
+			'topic_size' => 'Topic Size',
+			'link_color' => 'Link Color',
+			'link_size' => 'Link Size',
 		);
 	}
 
@@ -92,6 +106,12 @@ class WebShopFormatBase extends CActiveRecord
 		$criteria->compare('logo',$this->logo,true);
 		$criteria->compare('theme',$this->theme,true);
 		$criteria->compare('background',$this->background,true);
+		$criteria->compare('char_color',$this->char_color,true);
+		$criteria->compare('char_size',$this->char_size,true);
+		$criteria->compare('topic_color',$this->topic_color,true);
+		$criteria->compare('topic_size',$this->topic_size,true);
+		$criteria->compare('link_color',$this->link_color,true);
+		$criteria->compare('link_size',$this->link_size,true);
 
 		return new CActiveDataProvider($this, array(
 			'criteria'=>$criteria,
