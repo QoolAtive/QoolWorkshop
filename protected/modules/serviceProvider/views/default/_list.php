aeab80a3653b0c70f5059d989256f0110d809ef0
vasitjuntong <N_vasit@hotmail.com>
2013-08-15 16:38:24 +0700

    service provider_แก้ไขรูปภาพไม่ขึ้น

--- protected/modules/serviceProvider/views/default/_list.php
+++ protected/modules/serviceProvider/views/default/_list.php
@@ -4,7 +4,11 @@ $name = LanguageHelper::changeDB($data->name, $data->name_en);
 <ul style="display: inline-block;">
     <li>
         <a href="/serviceProvider/default/detail/id/<?php echo $data->id; ?>/type/<?php echo $_GET['id']; ?>">
-            <img src="/file/logo/<?php echo $data->logo; ?>" height="100">
+            <?php if ($data->logo == null) { ?>
+                <img src="/file/logo/default.jpg" height="100">
+            <?php } else { ?>
+                <img src="/file/logo/<?php echo $data->logo; ?>" height="100">
+            <?php } ?>
         </a>
     </li>
     <li>
