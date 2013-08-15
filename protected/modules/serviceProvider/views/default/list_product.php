1074168aefec6e0d61ea0d675ac09d46f4b10797
golfiti <golfitiz@gmail.com>
2013-08-15 18:06:38 +0700

    จัด style serviceproviderdetail
    - จัด style สินค้าใหม่
    - จัด style ข้อมูลบริษัท
    - logo ตรงกลางแนวตั้ง
    - ปุ่มแก้ไข

--- protected/modules/serviceProvider/views/default/list_product.php
+++ protected/modules/serviceProvider/views/default/list_product.php
@@ -2,31 +2,37 @@
 $name = LanguageHelper::changeDB($data->name, $data->name_en);
 $detail = LanguageHelper::changeDB($data->detail, $data->detail_en);
 ?>
-<ul style="display: inline-block;">
+<style type="text/css">
+
+</style>
+<ul class="productlist">
+
     <li>
-        <img src="/file/product/<?php echo $data->image; ?>" height="100">
+<img src="/file/product/<?php echo $data->image; ?>" alt="new product" width="100%">
+<!-- 
+  <img src="http://dev.dbd.qoolative.com/file/logo/default.jpg" width="100%"> -->
     </li>
-    <?php
+    
+    <li>
+        <label style="font-size: 16px; font-weight: bold;color: #D69500;"><?php echo $name; ?></label>
+                    <?php
     if (Yii::app()->user->isAdmin()) {
         ?>
-        <li style="padding: 0 25%;">
+        
             <?php
             echo CHtml::button(Yii::t('language', 'แก้ไข'), array(
-                'class' => "grey", // btnedit grey
+                'class' => "grey right", // btnedit grey
 //                'style' => 'margin-left: 669px; margin-top: 0px; position:absolute;',
                 'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/serviceProvider/manage/insertProduct/id/' . $_GET['id'] . '/pro_id/' . $data->id
                 )) . "'")
             );
             ?>
-        </li>
+       
         <?php
     }//end if (Yii::app()->user->isAdmin()) {
     ?>
-    <li>
-        <label><?php echo $name; ?></label>
-    </li>
-    <li>
+
         <label><?php echo $detail; ?></label>
     </li>
 
