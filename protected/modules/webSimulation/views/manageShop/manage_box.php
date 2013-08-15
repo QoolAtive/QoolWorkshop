54056a19937fb2f388a8d5a94b3442fc9b2794e7
golfiti <golfitiz@gmail.com>
2013-08-15 10:54:46 +0700

    add style web sim box

--- protected/modules/webSimulation/views/manageShop/manage_box.php
+++ protected/modules/webSimulation/views/manageShop/manage_box.php
@@ -4,55 +4,39 @@ $this->renderPartial('_side_menu', array('index' => 'shop'));
 
 <div class="content">
     <div class="tabcontents" >
-        <h3 class="_100">จัดการกล่อง </h3>
-        <hr>
+        <h3 class="barH3">
+            <span>
+                <i class="icon-shopping-cart"></i>
+                <a href="<?php echo CHtml::normalizeUrl(array("/webSimulation/manageShop/manageShop")); ?>">
+                    <?php
+                    $shop_name = WebShop::model()->findByPk($shop_id)->name_th;
+                    echo Yii::t('language', 'ร้าน ') . $shop_name;
+                    ?>
+                </a>
+                <i class="icon-chevron-right"></i>
+                <a href="<?php echo CHtml::normalizeUrl(array("/webSimulation/manageShop/manageShop")); ?>">
+                    <?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'ร้านค้า'); ?>
+                </a>
+                <i class="icon-chevron-right"></i>
+                <?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'กล่องแสดงสินค้า'); ?>
+            </span>
+        </h3>
+        
         <div class="_100">
             <a class="addboxbtn fancybox.ajax btn" href="<?php echo CHtml::normalizeUrl(array('/webSimulation/manageShop/addBox')); ?>"> เพิ่มกล่องแสดงสินค้า </a>
-            <a class="addboxbtn fancybox.ajax btn" href="web-sim-box-html.html">ใส่โค๊ด html </a>
-            <a class="addboxbtn fancybox.ajax btn" href="web-sim-box-video.html">วีดีโอ/เพลง</a>
+            <a class="addboxbtn fancybox.ajax btn" href="<?php echo CHtml::normalizeUrl(array('/webSimulation/manageShop/addHtml')); ?>">ใส่โค๊ด html </a>
+            <a class="addboxbtn fancybox.ajax btn" href="<?php echo CHtml::normalizeUrl(array('/webSimulation/manageShop/addVideo')); ?>">วีดีโอ/เพลง</a>
+            <a href="<?php echo CHtml::normalizeUrl(array('/webSimulation/manageShop/sortBox')); ?>">จัดลำดับกล่อง</a>
         </div>
         <hr>
 
-
-
-        <ul class="droptrue">
-            <li id="recordsArray_1"> 
-
-                <p class="headsort">Hot Product</p>
-                <p class="tool">
-                    <a href="">แก้ไขสินค้า</a>
-                    &nbsp;|&nbsp;
-                    <a href="" onclick="return confirm('Are you sure to Delete?');">ลบ</a>
-                    &nbsp;|&nbsp;
-                    <a href="#">แก้ไขการแสดงผล</a>
-                    &nbsp;|&nbsp;
-                    <a href="#" class="hideshowbox">แสดง/ซ่อน</a>
-
-                </p>
-
-            </li>
-            <li id="recordsArray_2">
-                <p class="headsort">New Release</p>
-
-
-                <p class="tool">
-                    <a href="">แก้ไขสินค้า</a>
-                    &nbsp;|&nbsp;
-                    <a href="" onclick="return confirm('Are you sure to Delete?');">ลบ</a>
-                    &nbsp;|&nbsp;
-                    <a href="#">แก้ไขการแสดงผล</a>
-                    &nbsp;|&nbsp;
-                    <a href="#" class="hideshowbox">แสดง/ซ่อน</a>
-                </p>
-
-
-            </li>
-
+        <ul class="droptrue left">
             <?php
             $boxs = WebShopBox::model()->findAll();
+            $i = 1;
             foreach ($boxs as $box) {
                 ?>
-                <li id="recordsArray_4">
+                <li id="recordsArray_<?php echo $i; ?>">
                     <p class="headsort"><?php echo $box['name_th']; ?></p>
                     <p class="tool">
                         <a href="">แก้ไขสินค้า</a>
@@ -66,13 +50,15 @@ $this->renderPartial('_side_menu', array('index' => 'shop'));
 
                     </p>
                 </li>
-            <?php } ?>
+            <?php 
+            $i += 1;
+            } ?>
         </ul>   
 
 
 
 
-        <ul class="droptrue" style="padding-top:14px;float: right; width:34%; padding: 10px; background: #eee;">
+        <ul class="droptrue right" style="padding-top:14px;float: right; width:34%; padding: 10px; background: #eee;">
             <li>
                 Calendar 
 
