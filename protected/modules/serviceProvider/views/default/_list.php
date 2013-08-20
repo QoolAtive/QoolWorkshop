d796e033d2c6af38fb77bb3fd6d9a959fa5e95a3
golfiti <golfitiz@gmail.com>
2013-08-20 11:02:23 +0700

    จัดขนาดรูปภาพ partner

--- protected/modules/serviceProvider/views/default/_list.php
+++ protected/modules/serviceProvider/views/default/_list.php
@@ -1,17 +1,17 @@
 <?php
 $name = LanguageHelper::changeDB($data->name, $data->name_en);
 ?>
-<ul style="display: inline-block;">
+<ul class="listpartner">
     <li>
         <a href="/serviceProvider/default/detail/id/<?php echo $data->id; ?>/type/<?php echo $_GET['id']; ?>">
             <?php if ($data->logo == null) { ?>
-                <img src="/file/logo/default.jpg" height="100">
+                <img src="/file/logo/default.jpg">
             <?php } else { ?>
-                <img src="/file/logo/<?php echo $data->logo; ?>" height="100">
+                <img src="/file/logo/<?php echo $data->logo; ?>" alt="partnerlogo">
             <?php } ?>
         </a>
     </li>
-    <li style='text-align: center;'>
+    <li>
         <a href="/serviceProvider/default/detail/id/<?php echo $data->id; ?>/type/<?php echo $_GET['id']; ?>">
             <label><?php echo $name; ?></label>
         </a>
