fb85e74ccdf565d8bad7e981b1ad4c03125ec342
annbluesky <ann.kanowan@gmail.com>
2013-08-22 15:50:04 +0700

    Service Provider: ส่วน Admin เมนู แก้ไขคำในการแปลภาษา

--- protected/modules/serviceProvider/views/manage/_side_bar.php
+++ protected/modules/serviceProvider/views/manage/_side_bar.php
@@ -48,7 +48,7 @@
 
                 if ($select3 != null) {
                     echo "<li>";
-                    echo CHtml::link(Yii::t('language', 'เพิ่มสินค้าและบริการ'), array(
+                    echo CHtml::link(Yii::t('language', 'เพิ่ม').Yii::t('language', 'สินค้าและบริการ'), array(
                         '#'), array(
                         'rel' => 'view5',
                         'class' => $select3 == 'selected' ? 'menuactive listactive' : ''
