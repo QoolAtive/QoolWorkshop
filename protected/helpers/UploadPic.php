5cda32c080263708b6a8689578bfbcbdc18fd115
Piyanan <pyn@live.com>
2013-08-23 16:11:11 +0700

    แก้กลับเป็นอย่างเดิม

--- protected/helpers/UploadPic.php
+++ protected/helpers/UploadPic.php
@@ -6,9 +6,6 @@ class UploadPic {
         $fileSave = CUploadedFile::getInstance($model, $attribute);
         if ($fileSave != NULL) {
             $dir = '/upload/img/websim/item/';
-            if (!is_dir($dir)) {
-                mkdir($dir, 0755, true);
-            }
             if ($model->$attribute != NULL) {// ถ้ามีไฟล์อัพมาใหม่ ต้องลบไฟลเก่าก่อน แล้วค่อยอัพไฟล์ใหม่กลับเข้าไป
                 if (fopen('.' . $model->$attribute, 'w'))
                     if (unlink('.' . $model->$attribute)) {
