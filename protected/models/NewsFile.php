e60fe24970cfd2b16352c53b783d23038e3b3549
annbluesky <ann.kanowan@gmail.com>
2013-08-23 14:42:31 +0700

    News: เพิ่มตารางเก็บไฟล์ที่แนบมากับข่าว

---
+++ protected/models/NewsFile.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * This is the model class for table "news_file".
+ *
+ * The followings are the available columns in table 'news_file':
+ * @property string $news_file_id
+ * @property integer $news_id
+ * @property string $file_name
+ * @property string $file_path
+ *
+ * The followings are the available model relations:
+ * @property News $news
+ */
+class NewsFile extends NewsFileBase
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return NewsFile the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('news_id, file_name, file_path', 'required'),
+			array('news_id', 'numerical', 'integerOnly'=>true),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('news_file_id, news_id, file_name, file_path', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'news' => array(self::BELONGS_TO, 'News', 'news_id'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'news_file_id' => 'News File',
+			'news_id' => 'News',
+			'file_name' => 'File Name',
+			'file_path' => 'File Path',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('news_file_id',$this->news_file_id,true);
+		$criteria->compare('news_id',$this->news_id);
+		$criteria->compare('file_name',$this->file_name,true);
+		$criteria->compare('file_path',$this->file_path,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
