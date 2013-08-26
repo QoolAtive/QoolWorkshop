a9280d3f294b936e8d5b64cb0a5d3e7a25f1121b
golfiti <golfitiz@gmail.com>
2013-08-26 17:20:07 +0700

    แก้หน้าแรกเป็น spite image เพื่อไม่ให้โหลดรูปเมื่อ hover

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,14 +1,18 @@
 <!--left side-->
 <div class="colleft">
     <ul class="gridmenu">
+        <div class="clearfix">
         <li class="knowledge"><a href="/knowledge/default/index"></a></li>
         <li class="websim"><a href="/webSimulation/default/index"></a></li>
         <li class="edir"><a href="/eDirectory/default/index"></a></li>
         <li class="servicepro"><a href="/serviceProvider/default/index"></a></li>
+</div>
+<div  class="clearfix">
         <li class="link"><a href="/link/default/index"></a></li>
         <li class="faq"><a href="/faq/default/index"></a></li>
         <li class="aboutus"><a href="/about/default/index"></a></li>
         <li class="news"><a href="/news/default/index"></a></li>
+    </div>
     </ul>
 </div>
 <!--/left side-->
