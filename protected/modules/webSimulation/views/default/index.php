fdc816a1e697481e8bdc4b9a9a62213354c91e55
Piyanan <pyn@live.com>
2013-08-26 16:51:38 +0700

    websim: เอาเมนูจัดการร้านค้าหน้าแนะนำออก

--- protected/modules/webSimulation/views/default/index.php
+++ protected/modules/webSimulation/views/default/index.php
@@ -3,21 +3,19 @@
         <ul>
             <li class="boxhead"><img src="<?php echo Yii::t('language', '/img/iconpage/websim.png'); ?>"/></li>
         </ul>
-        <ul class="tabs clearfix">
-            <li>
-                <?php
-                if (isset(Yii::app()->user->id)) {
-                    ?>
+
+        <?php
+        if (isset(Yii::app()->user->id)) {
+            ?>
+            <ul class="tabs clearfix">
+                <li>
                     <a href="<?php echo CHtml::normalizeUrl(array("/webSimulation/manageShop/manageShopList")); ?>">
-                        <?php
-                    } else {
-                        ?>
-                        <a href="#" onclick="alert('<?php echo Yii::t('language', 'กรุณาเข้าระบบก่อนใช้งาน'); ?>');">
-                            <?php
-                        }
-                        ?>
                         <?php echo Yii::t('language', 'จัดการรายการร้านค้าของคุณ'); ?>
-                    </a></li>
+                    </a>
+                </li>
+            </ul>
+        <?php }
+        ?>
 
     </div>
 </div>
@@ -26,46 +24,46 @@
         <div class="websimborder" style="min-height: 400px;">
 
             <h3 class="textcenter" style="padding-top: 20px;">Web Simulation</h3>
-                        <p class="textcenter">ระบบแนะนำการทดลองการเปิดร้านค้าออนไลน์</p>
+            <p class="textcenter">ระบบแนะนำการทดลองการเปิดร้านค้าออนไลน์</p>
 
             <div style="padding:20px 100px">
-            <p>คำแนะนำการใช้งานระบบ</p>
-                        <p style="text-indent: 20px;"> - ระบบนี้เป็นระบบจำลองเปิดร้านค้าออนไลน์ เพื่อให้ผู้สนใจ มีความรู้ความเข้าใจในการเปิดร้านค้าออนไลน์</p>
-                        <!-- <p style="text-indent: 20px;"> - </p> -->
+                <p>คำแนะนำการใช้งานระบบ</p>
+                <p style="text-indent: 20px;"> - ระบบนี้เป็นระบบจำลองเปิดร้านค้าออนไลน์ เพื่อให้ผู้สนใจ มีความรู้ความเข้าใจในการเปิดร้านค้าออนไลน์</p>
+                <!-- <p style="text-indent: 20px;"> - </p> -->
 
 
             </div>
 
-        <div class="textcenter" >
+            <div class="textcenter" >
                 <?php
-                    if (isset(Yii::app()->user->id)) {
-                ?>
+                if (isset(Yii::app()->user->id)) {
+                    ?>
                     <div class="_100">
-                    <input type="button" value="Start Simulation" name="yt1" onclick="window.location='/webSimulation/manageShop/register'">
+                        <input type="button" value="Start Simulation" name="yt1" onclick="window.location = '/webSimulation/manageShop/register'">
                     </div>
-    <!--             echo CHtml::link(Yii::t('language', 'Start Simulation'), CHtml::normalizeUrl(array("/webSimulation/manageShop/register")), array(
-                    'style' => "border-radius: 4px;  margin: 0 auto; vertical-align: middle; line-height:30px; display: block; width: 200px; border: 1px solid black;",
-                ));
- -->            <?php
-                    } else {
+                    <!--             echo CHtml::link(Yii::t('language', 'Start Simulation'), CHtml::normalizeUrl(array("/webSimulation/manageShop/register")), array(
+                                    'style' => "border-radius: 4px;  margin: 0 auto; vertical-align: middle; line-height:30px; display: block; width: 200px; border: 1px solid black;",
+                                ));
+                    -->            <?php
+                } else {
+                    ?>
+
+                    <div class="_100">
+                        <input type="button" value="<?php echo Yii::t('language', 'กรุณาเข้าระบบก่อนใช้งาน'); ?>" name="login" onclick="alert('<?php echo Yii::t('language', 'กรุณาเข้าระบบก่อนใช้งาน'); ?>');">
+                    </div>
+
+
+                    <!-- echo CHtml::link(Yii::t('language', 'Start Simulation'), '#', array(
+                        'style' => "border-radius: 4px;  margin: 0 auto; vertical-align: middle; line-height:30px; display: block; width: 200px; border: 1px solid black;",
+                        'onclick' => 'alert("' . Yii::t('language', 'กรุณาเข้าระบบก่อนใช้งาน') . '");',
+                    )); -->
+                    <?php
+                }
                 ?>
 
-                <div class="_100">
-                    <input type="button" value="กรุณาเข้าระบบก่อนใช้งาน"name="login">
-                </div>
+                <img src="/img/smart.png" style="padding: 60px 0px;">
 
-
-                <!-- echo CHtml::link(Yii::t('language', 'Start Simulation'), '#', array(
-                    'style' => "border-radius: 4px;  margin: 0 auto; vertical-align: middle; line-height:30px; display: block; width: 200px; border: 1px solid black;",
-                    'onclick' => 'alert("' . Yii::t('language', 'กรุณาเข้าระบบก่อนใช้งาน') . '");',
-                )); -->
-                <?php
-            }
-            ?>
-
-            <img src="/img/smart.png" style="padding: 60px 0px;">
-
+            </div>
         </div>
-</div>
     </div>
 </div>
