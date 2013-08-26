3fa0c8d0ea7df9df9bdcdec264ad3aaadc223fce
annbluesky <ann.kanowan@gmail.com>
2013-08-26 18:10:59 +0700

    E-directory: ส่วน Admin แก้ไข หน้าแสดงสินค้า

--- protected/modules/eDirectory/views/admin/product.php
+++ protected/modules/eDirectory/views/admin/product.php
@@ -1,32 +1,59 @@
 <?php
-$list = array(
-    array('text' => Yii::t('language', 'ข้อมูลทั้งหมด'), 'link' => '/eDirectory/admin/index', 'select' => ''),
-    array('text' => Yii::t('language', 'จัดการสินค้าและบริการ'), 'link' => '#', 'select' => 'selected'),
-);
 $this->renderPartial('side_bar', array(
-    'list' => $list,
+    'active' => 5,
+    'company_id' => $id,
 ));
+$title = Company::model()->find('id=:id', array(':id' => $id));
+$company_name = LanguageHelper::changeDB($title->name, $title->name_en);
 ?>
 <div class="content">
     <div class="tabcontents">
+        <h3 class="barH3">
+            <span>
+                <i class="icon-home"></i>
+                <?php
+                echo CHtml::link(Yii::t('language', 'จัดการ') . Yii::t('language', 'ร้านค้า'), array('/eDirectory/admin/index'));
+                ?>
+                <i class="icon-chevron-right"></i>
+                <?php echo CHtml::link(Yii::t('language', 'ร้านค้าทั้งหมด'), array('/eDirectory/admin/index')); ?>
+                <i class="icon-chevron-right"></i>
+                <?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'สินค้าและบริการ') . ' (' . $company_name . ') '; ?>
+            </span>
+        </h3>
         <div class="textcenter">
+            <hr>
             <?php
-            echo CHtml::button(Yii::t('language', 'เพิ่มข้อมูลสินค้า'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+            echo CHtml::button(Yii::t('language', 'เพิ่ม') . Yii::t('language', 'ข้อมูลสินค้า'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/eDirectory/admin/insertProduct/id/' . $id // id = id บริษัท
                 )) . "'")
             );
-            echo CHtml::button(Yii::t('language', 'อัพโหลดสินค้า'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+            echo CHtml::button(Yii::t('language', 'อัพโหลด') . Yii::t('language', 'สินค้า'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/eDirectory/admin/companyProductUpload/company_id/' . $id // id = id บริษัท
                 )) . "'")
             );
             ?>
+            <hr>
         </div>
-        <hr>
-        <?php
-        $this->renderPartial('product_grid', array(
-            'dataProvider' => $dataProvider,
-            'model' => $model,
-        ));
-        ?>
+        <div>
+            <?php
+            $this->renderPartial('product_grid', array(
+                'dataProvider' => $dataProvider,
+                'model' => $model,
+            ));
+            ?>
+        </div>
+        <div class="_100 textcenter">
+            <hr>
+            <?php
+            echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
+                    '/eDirectory/admin/index'
+                )) . "'")
+            );
+            ?>
+            <hr>
+        </div>
     </div>
 </div>
