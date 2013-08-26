3d6da51038e24ef067f919050c369c4372e098a6
annbluesky <ann.kanowan@gmail.com>
2013-08-26 17:58:50 +0700

    E-directory: ส่วน Admin แก้ไขส่วนเมนู

--- protected/modules/eDirectory/views/admin/side_bar.php
+++ protected/modules/eDirectory/views/admin/side_bar.php
@@ -1,19 +1,73 @@
 <div class="sidebar">
     <div class="menuitem">
-        <ul>
-            <li class="boxhead"><a href="/eDirectory/default/index"><img src="/img/iconpage/edir.png"/></a></li>
+        <ul>           
+            <li class="boxhead" style="background: url('<?php echo Yii::t('language', '/img/iconpage/edir.png'); ?>'); background-size: 227px; margin-left: -1px; " ></li>
         </ul>
         <ul class="tabs clearfix">
-<!--            <li><a href="#" rel="view1">All</a></li>
-            <li><a href="#" rel="view2">Tourism</a></li>
-            <li><a href="#" rel="view3">Wholesal</a></li>
-            <li><a href="#" rel="view4">Service</a></li>
-            <li><a href="#" rel="view5">Franchise</a></li>
-            <li><a href="#" rel="view6">ETC</a></li>
-            <li><a href="#" rel="view7">Show Share</a></li>
-            <li><a href="#" rel="view8">My Directory</a></li>-->
             <?php
-            Tool::GenList($list);
+            echo "<li>";
+            echo CHtml::link(Yii::t('language', 'ร้านค้าทั้งหมด'), array('/eDirectory/default/index'), array('rel' => 'view1'));
+            echo "</li>";
+            if (Yii::app()->user->isAdmin()) {
+                echo '<li>' . CHtml::link(Yii::t('language', 'จัดการ') . Yii::t('language', 'ร้านค้า'), array('/eDirectory/admin/index'), array('rel' => 'view2', 'class' => 'selected')) . '</li>';
+            }
+            ?>
+        </ul>
+        <div class="textcenter" style="   background: none repeat scroll 0 0 #F1F1F1;
+             border-top: 2px solid gold;
+             font-size: 16px;
+             margin-top: 6px;
+             padding: 14px 0;">
+            <p style="font-weight: bold;">
+                <?php
+                echo Yii::t('language', 'จัดการ') . Yii::t('language', 'ร้านค้า');
+                ?>
+            </p>
+        </div>
+        <ul class="rectangle-list">
+            <p class="demoline"></p>
+            <?php
+            echo "<li>";
+            echo CHtml::link(Yii::t('language', 'ร้านค้าทั้งหมด'), array(
+                '/eDirectory/admin/index'), array(
+                'rel' => 'admin1',
+                'class' => $active == '1' ? 'menuactive listactive' : ''
+            ));
+            echo "</li>";
+
+            echo "<li>";
+            echo CHtml::link(Yii::t('language', 'ร้านค้าที่ยังไม่ได้รับการอนุมัติ'), array(
+                '/eDirectory/admin/companyWaiting'), array(
+                'rel' => 'admin2',
+                'class' => $active == '2' ? 'menuactive listactive' : ''
+            ));
+            echo "</li>";
+
+            echo "<li>";
+            echo CHtml::link(Yii::t('language', 'ความเคลื่อนไหว'), array(
+                '/eDirectory/admin/companyMotion'), array(
+                'rel' => 'admin3',
+                'class' => $active == '3' ? 'menuactive listactive' : ''
+            ));
+            echo "</li>";
+
+            echo "<li>";
+            echo CHtml::link(Yii::t('language', 'ตั้งค่าความเคลื่อนไหว'), array(
+                '/eDirectory/admin/motionSetting'), array(
+                'rel' => 'admin4',
+                'class' => $active == '4' ? 'menuactive listactive' : ''
+            ));
+            echo "</li>";
+
+            if ($active == 5 && $company_id != '') {
+                echo "<li>";
+                echo CHtml::link(Yii::t('language', 'จัดการ') . Yii::t('language', 'สินค้าและบริการ'), array(
+                    '/eDirectory/admin/product/id/' . $company_id), array(
+                    'rel' => 'admin5',
+                    'class' => $active == '5' ? 'menuactive listactive' : ''
+                ));
+                echo "</li>";
+            }
             ?>
         </ul>
     </div>
