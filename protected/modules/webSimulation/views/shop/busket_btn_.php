d4cb0e923783e0b4b9072015c054404213f7fd45
Piyanan <pyn@live.com>
2013-08-29 15:43:49 +0700

    แก้ปุ่มajax หยิบสินค้าลงตะกร้า

--- protected/modules/webSimulation/views/shop/busket_btn_.php
+++ protected/modules/webSimulation/views/shop/busket_btn_.php
@@ -1,29 +1,40 @@
 <?php
-$form = $this->beginWidget('CActiveForm', array(
-    'id' => 'select-form',
-        ));
+
 if ($busket[$item_id] == NULL) {
+    $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'select-form',
+    ));
     echo Yii::t('language', 'จำนวน');
     echo ' ' . CHtml::textField('number', '1', array(
         'class' => 'numberinput'
     )) . ' ';
     echo Yii::t('language', 'ชิ้น');
+    echo CHtml::ajaxSubmitButton('หยิบใส่ตะกร้า', '/webSimulation/shop/selectItem/item_id/' . $item_id, array(
+//        'update' => '#busket_btn',
+        'type' => 'POST',
+        'dataType' => 'json',
+        'success' => "function (data){
+            $('#busket_btn').html(data.div1);
+            $('#busket_side').html(data.div2);
+        }"
+            ), array(
+        'id' => 'select',
+    ));
+    $this->endWidget();
 } else {
     echo Yii::t('language', 'จำนวนในตะกร้า');
     echo ' ' . $busket[$item_id] . ' ';
     echo Yii::t('language', 'ชิ้น');
-}
-echo CHtml::ajaxSubmitButton('หยิบออกจากตะกร้า', '/webSimulation/shop/selectItem/item_id/' . $item_id, array(
+    echo CHtml::ajaxSubmitButton('หยิบออกจากตะกร้า', '/webSimulation/shop/selectItem/item_id/' . $item_id, array(
 //        'update' => '#busket_btn',
-    'type' => 'POST',
-    'dataType' => 'json',
-    'success' => "function (data){
+        'type' => 'POST',
+        'dataType' => 'json',
+        'success' => "function (data){
             $('#busket_btn').html(data.div1);
             $('#busket_side').html(data.div2);
         }"
-        ), array(
-    'id' => 'select',
-));
-$this->endWidget();
+            ), array(
+        'id' => 'select',
+    ));
+}
 ?>
-<!--<div id="busket_side"></div>-->
