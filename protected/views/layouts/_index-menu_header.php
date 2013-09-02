fbf39c346621dc29f88c86292f8789c8b92b1f8c
annbluesky <ann.kanowan@gmail.com>
2013-09-02 11:32:56 +0700

    Search: แก้ไข layouts

--- protected/views/layouts/_index-menu_header.php
+++ protected/views/layouts/_index-menu_header.php
@@ -4,35 +4,33 @@
             <?php
             if (!Yii::app()->user->id) {
                 ?>
-                <a class="createaccountbtn fancybox" href="/member/manage/registerRules">Create Account</a> 
-                <a class="loginbtn fancybox fclogin" href="/site/login">Login</a>
+                <a class="createaccountbtn fancybox" href="/member/manage/registerRules">
+                    <?php echo Yii::t('language', 'สร้างผู้ใช้งาน'); ?>
+                </a> 
+                <a class="loginbtn fancybox fclogin" href="/site/login">
+                    <?php echo Yii::t('language', 'เข้าระบบ'); ?>
+                </a>
                 <?php
             } else {
                 $profile = Tool::getProfile();
                 ?>
-                <a href="/site/logout" class="loginbtn " onClick="return confirm('<?php echo Yii::t('language', 'คุณต้องการออกจากระบบหรือไม่?'); ?>')">Logout</a>
-                <a href="/member/manage/profile" class="loginbtn " ><?php echo $profile['name']; ?></a>
+                <a href="/site/logout" class="loginbtn " onClick="return confirm('<?php echo Yii::t('language', 'คุณต้องการออกจากระบบหรือไม่?'); ?>')">
+                    <?php echo Yii::t('language', 'ออกจากระบบ'); ?>
+                </a>
+                <a href="/member/manage/profile" class="loginbtn " >
+                    <?php echo $profile['name']; ?>
+                </a>
                 <?php
             }
             ?>
         </li>
 
-        <li class="clearfix">
-            <!--<input class="searchbox" placeholder="Search" type="text" name="" value="" />--> 
-            <?php
-            // google_search
-            $this->widget('application.extensions.search.GoogleSearch');
-//            $this->widget('ext.esearch.SearchBoxPortlet');
-//or
-//            SearchAction::renderInputBox();
-//or
-//            $this->renderPartial('extensions/esearch/views/inputBox.php');
-            ?>
-        </li>
-
         <li class="clearfix " style="width: 100%;">
-            <a href="#"><img class="rightindex" alt="EN" src="/img/en.png"></a>
-            <a href="#"><img class="rightindex" alt="TH" src="/img/th.png"></a>
+            <div class="lang txt-right"> 
+                <?php
+                $this->widget('application.components.widgets.LanguageSelector');
+                ?>
+            </div>
         </li>
     </ul> 
 </div>
