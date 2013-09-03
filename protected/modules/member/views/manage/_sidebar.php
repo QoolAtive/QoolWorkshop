67c84f5aa57890ec3e299618003aee66902e9107
annbluesky <ann.kanowan@gmail.com>
2013-09-03 11:22:24 +0700

    Google Analytics: เพิ่มปุ่ม และใส่ Function แปลภาษา

--- protected/modules/member/views/manage/_sidebar.php
+++ protected/modules/member/views/manage/_sidebar.php
@@ -1,25 +1,25 @@
 <div class="sidebar">
     <div class="menuitem">
         <ul>
-            <li class="boxhead"><img src="/img/iconpage/Member.png"/></li>
+            <li class="boxhead"><img src="<?php echo Yii::t("language", '/img/iconpage/Member.png'); ?>"/></li>
         </ul>
         <ul class="tabs clearfix">
-            <li><a href="/member/manage/profile" >ข้อมูลของคุณ</a></li>
+            <li><a href="/member/manage/profile" ><?php echo Yii::t("language", 'ข้อมูลของคุณ'); ?></a></li>
             <?php if (!Yii::app()->user->isAdmin()) { ?>
-                <li><a href="/member/manage/changeAddress" >แก้ไขที่อยู่</a></li>
+                <li><a href="/member/manage/changeAddress" ><?php echo Yii::t("language", 'แก้ไขที่อยู่'); ?></a></li>
                 <?php
                 if (Yii::app()->user->isMemberType() == 2) {
                     ?>
-                    <li><a href="/member/manage/editMemberRegistration" >แก้ไขข้อมูลส่วนตัว</a></li>
+                    <li><a href="/member/manage/editMemberRegistration" ><?php echo Yii::t("language", 'แก้ไขข้อมูลส่วนตัว'); ?></a></li>
                     <?php
                 } else
                 if (Yii::app()->user->isMemberType() == 1) {
                     ?>
-                    <li><a href="/member/manage/editMemberPerson" >แก้ไขข้อมูลส่วนตัว</a></li>
+                    <li><a href="/member/manage/editMemberPerson" ><?php echo Yii::t("language", 'แก้ไขข้อมูลส่วนตัว'); ?></a></li>
                     <?php
                 }
                 ?>
-                <li><a href="/member/manage/changePassword" >แก้ไขรหัสผ่าน</a></li>
+                <li><a href="/member/manage/changePassword" ><?php echo Yii::t("language", 'แก้ไขรหัสผ่าน'); ?></a></li>
                 <?php
             }
             if (Yii::app()->user->isMemberType() == 1) {
@@ -27,12 +27,13 @@
             } else {
                 if (!Yii::app()->user->isAdmin()) {
                     ?>
-                    <li><a href="/eDirectory/manage/index" >ใช้งานร้านค้า</a></li>
+                    <li><a href="/eDirectory/manage/index" ><?php echo Yii::t("language", 'ใช้งานร้านค้า'); ?></a></li>
                     <?php
                 }
             }
             ?>
-            <li><a href="/serviceProvider/default/spLog" >บริการโปรด</a></li>
+            <li><a href="/serviceProvider/default/spLog" ><?php echo Yii::t("language", 'บริการโปรด'); ?></a></li>
+            <li><a href="http://www.google.com/intl/<?php echo Yii::t("language", 'th'); ?>/analytics/" target="_blank" ><?php echo Yii::t("language", 'สถิติของเว็บไซต์'); ?></a></li>
         </ul>
     </div>
 </div>
