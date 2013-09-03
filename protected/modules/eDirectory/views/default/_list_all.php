22cfe853aa867b5297bd73b94a645656be7344b9
golfiti <golfitiz@gmail.com>
2013-09-03 14:34:59 +0700

    live-error_04

--- protected/modules/eDirectory/views/default/_list_all.php
+++ protected/modules/eDirectory/views/default/_list_all.php
@@ -16,16 +16,7 @@ $model_type_com = CompanyType::model()->findAll("company_id ='" . $data->id . "'
     </h4>
     <ul>
 
-        <?php 
-            if(empty($logo)){ 
-        ?>
-            <li><img alt="e-dirshoplogo"  src="/file/knowledge/default.jpg"/></li> 
-        <?php 
-            } else{
-        ?>
-           <li><img alt="e-dirshoplogo"  src="/file/logo/<?php echo $data->logo; ?>"/></li>
-        <?  }  ?>
-
+       
         <li>
             <i class="icon-tags"></i> <label><?php echo Yii::t('language', 'ประเภทร้านค้า') . " : "; ?></label>
             <?php
