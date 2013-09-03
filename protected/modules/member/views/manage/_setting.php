d6b18a924b5da4c7dcd7bc2d3aa3fe18e1e8888b
annbluesky <ann.kanowan@gmail.com>
2013-09-03 12:16:12 +0700

    Member: แก้ไข/ เพิ่ม Function แปลภาษา

--- protected/modules/member/views/manage/_setting.php
+++ protected/modules/member/views/manage/_setting.php
@@ -1,10 +1,42 @@
 <ul class="btnMangae _100">
-    <li><?php echo CHtml::link('ระดับการศึกษา', array('/dataCenter/default/highEducation')); ?></li>
-    <li><?php echo CHtml::link('ประเภทร้านค้า', array('/dataCenter/default/companyTypeBusiness')); ?></li>
-    <li><?php echo CHtml::link('เพศ', array('/dataCenter/default/sex')); ?></li>
-    <li><?php echo CHtml::link('คำนำหน้า', array('/dataCenter/default/titleName')); ?></li>
-    <li><?php echo CHtml::link('Title Web', array('/dataCenter/default/titleWeb')); ?></li>
-    <li><?php echo CHtml::link('Description', array('/dataCenter/default/description')); ?></li>
-    <li><?php echo CHtml::link('Keyword', array('/dataCenter/default/keyword')); ?></li>
-    <li><?php echo CHtml::link('Site Map', array('/dataCenter/default/siteMap')); ?></li>
+    <li>
+        <?php
+        echo CHtml::link(Yii::t("language", 'ระดับการศึกษา'), array('/dataCenter/default/highEducation'));
+        ?>
+    </li>
+    <li>
+        <?php
+        echo CHtml::link(Yii::t("language", 'ประเภทร้านค้า'), array('/dataCenter/default/companyTypeBusiness'));
+        ?>
+    </li>
+    <li>
+        <?php
+        echo CHtml::link(Yii::t("language", 'เพศ'), array('/dataCenter/default/sex'));
+        ?>
+    </li>
+    <li>
+        <?php
+        echo CHtml::link(Yii::t("language", 'คำนำหน้า'), array('/dataCenter/default/titleName'));
+        ?>
+    </li>
+    <li>
+        <?php
+        echo CHtml::link(Yii::t("language", 'ชื่อเว็บ'), array('/dataCenter/default/titleWeb'));
+        ?>
+    </li>
+    <li>
+        <?php
+        echo CHtml::link(Yii::t("language", 'รายละเอียด'), array('/dataCenter/default/description'));
+        ?>
+    </li>
+    <li>
+        <?php
+        echo CHtml::link(Yii::t("language", 'คำสำคัญ'), array('/dataCenter/default/keyword'));
+        ?>
+    </li>
+    <li>
+        <?php
+        echo CHtml::link(Yii::t("language", 'แผนที่เว็บไซต์'), array('/dataCenter/default/siteMap'));
+        ?>
+    </li>
 </ul>
