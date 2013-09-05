6a9f559fc83ebba2b6105956805bd1f03f1cf21f
Piyanan <pyn@live.com>
2013-09-05 14:24:20 +0700

    เพิ่ม model หมวดหมู่หลัก/รอง

--- protected/models/FaqMain.php
+++ protected/models/FaqMain.php
@@ -7,80 +7,78 @@
  * @property integer $id
  * @property string $name
  */
-class FaqMain extends FaqMainBase
-{
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return FaqMainBase the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+class FaqMain extends FaqMainBase {
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'faq_main';
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return FaqMainBase the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('name', 'required'),
-			array('name', 'length', 'max'=>255),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, name', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'faq_main';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('name_th, name_en', 'required'),
+            array('name_th, name_en', 'length', 'max' => 255),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, name_th, name_en', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'name' => 'Name',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'faqQuestions' => array(self::HAS_MANY, 'FaqQuestion', 'fm_id'),
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id' => 'ID',
+            'name_th' => 'ชื่อหมวดหมู่หลักภาษาไทย',
+            'name_en' => 'ชื่อหมวดหมู่หลักภาษาอังกฤษ',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search() {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('name',$this->name,true);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria->compare('id', $this->id);
+        $criteria->compare('name_th', $this->name_th, true);
+        $criteria->compare('name_en', $this->name_en, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
 }
--- protected/models/FaqSub.php
+++ protected/models/FaqSub.php
@@ -1,22 +1,23 @@
 <?php
 
 /**
- * This is the model class for table "faq_main".
+ * This is the model class for table "faq_sub".
  *
- * The followings are the available columns in table 'faq_main':
- * @property integer $id
+ * The followings are the available columns in table 'faq_sub':
+ * @property integer $faq_sub_id
+ * @property integer $faq_main_id
  * @property string $name_th
  * @property string $name_en
  *
  * The followings are the available model relations:
- * @property FaqQuestion[] $faqQuestions
+ * @property FaqMain $faqMain
  */
-class FaqMainBase extends CActiveRecord
+class FaqSub extends FaqSubBase
 {
 	/**
 	 * Returns the static model of the specified AR class.
 	 * @param string $className active record class name.
-	 * @return FaqMainBase the static model class
+	 * @return FaqSubBase the static model class
 	 */
 	public static function model($className=__CLASS__)
 	{
@@ -28,7 +29,7 @@ class FaqMainBase extends CActiveRecord
 	 */
 	public function tableName()
 	{
-		return 'faq_main';
+		return 'faq_sub';
 	}
 
 	/**
@@ -39,11 +40,12 @@ class FaqMainBase extends CActiveRecord
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
 		return array(
-			array('name_th, name_en', 'required'),
+			array('faq_main_id, name_th, name_en', 'required'),
+			array('faq_main_id', 'numerical', 'integerOnly'=>true),
 			array('name_th, name_en', 'length', 'max'=>255),
 			// The following rule is used by search().
 			// Please remove those attributes that should not be searched.
-			array('id, name_th, name_en', 'safe', 'on'=>'search'),
+			array('faq_sub_id, faq_main_id, name_th, name_en', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -55,7 +57,7 @@ class FaqMainBase extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-			'faqQuestions' => array(self::HAS_MANY, 'FaqQuestion', 'fm_id'),
+			'faqMain' => array(self::BELONGS_TO, 'FaqMain', 'faq_main_id'),
 		);
 	}
 
@@ -65,9 +67,10 @@ class FaqMainBase extends CActiveRecord
 	public function attributeLabels()
 	{
 		return array(
-			'id' => 'ID',
-			'name_th' => 'Name Th',
-			'name_en' => 'Name En',
+			'faq_sub_id' => 'Faq Sub',
+			'faq_main_id' => 'Faq Main',
+			'name_th' => 'ชื่อหมวดหมู่ย่อยภาษาไทย',
+			'name_en' => 'ชื่อหมวดหมู่ย่อยภาษาอังกฤษ',
 		);
 	}
 
@@ -82,7 +85,8 @@ class FaqMainBase extends CActiveRecord
 
 		$criteria=new CDbCriteria;
 
-		$criteria->compare('id',$this->id);
+		$criteria->compare('faq_sub_id',$this->faq_sub_id);
+		$criteria->compare('faq_main_id',$this->faq_main_id);
 		$criteria->compare('name_th',$this->name_th,true);
 		$criteria->compare('name_en',$this->name_en,true);
 
