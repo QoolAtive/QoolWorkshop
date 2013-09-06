f540493c29de3595a72ea73ab63dd1c7445dc00a
annbluesky <ann.kanowan@gmail.com>
2013-09-06 11:09:12 +0700

    Data Center: เมนู แก้ไขคำสำหรับใช้ในการแปลภาษา

--- protected/modules/dataCenter/views/default/sex.php
+++ protected/modules/dataCenter/views/default/sex.php
@@ -1,39 +1,46 @@
 <?php
-$this->renderPartial('_sidebar', array());
+$this->renderPartial('_sidebar', array(
+    'selectSex' => 'selected',
+    ));
 ?>
 
 <div class="content">
     <div class="tabcontents">
-                <h3 class="barH3">
-        <span>
-             <i class="icon-cog"></i> 
-            <a href="<?php echo CHtml::normalizeUrl(array("/member/manage/profile")); ?>">
-             ตั้งค่าเว็บไซต์
-            </a>
-            <i class="icon-chevron-right"></i><?php echo Yii::t('language', 'เพศ'); ?>
-        </span>
+        <h3 class="barH3">
+            <span>
+                <i class="icon-cog"></i> 
+                <a href="<?php echo CHtml::normalizeUrl(array("/member/manage/profile")); ?>">
+                    <?php echo Yii::t('language', 'ตั้งค่าเว็บไซต์'); ?>
+                </a>
+                <i class="icon-chevron-right"></i> 
+                <?php echo Yii::t('language', 'เพศ'); ?>
+            </span>
         </h3>
-    
-    <div style="text-align: center;">
+
+        <div style="text-align: center;">
+            <?php
+            echo CHtml::button(Yii::t('language', 'เพิ่ม') . Yii::t('language', 'เพศ'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
+                    '/dataCenter/default/insertSex'
+                )) . "'")
+            );
+            ?>
+            <hr>
+        </div>
         <?php
-        echo CHtml::button(Yii::t('language', 'เพิ่มเพศ'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
-                '/dataCenter/default/insertSex'
-            )) . "'")
-        );
+        $this->renderPartial('_grid_sex', array(
+            'model' => $model,
+        ));
         ?>
+        <div style="text-align: center;">
+            <hr>
+            <?php
+            echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+                    '/member/manage/profile'
+                )) . "'")
+            );
+            ?>
+            <hr>
+        </div>
     </div>
-    <?php
-    $this->renderPartial('_grid_sex', array(
-        'model' => $model,
-    ));
-    ?>
-    <div style="text-align: center;">
-        <?php
-        echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
-                '/member/manage/profile'
-            )) . "'")
-        );
-        ?>
-    </div>
-</div>
 </div>
