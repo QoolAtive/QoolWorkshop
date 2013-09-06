b73bfa8cb8572d7bb49e73327fbbdbf52d185770
annbluesky <ann.kanowan@gmail.com>
2013-09-06 11:08:55 +0700

    Data Center: ประเภทร้านค้า แก้ไขคำสำหรับใช้ในการแปลภาษา

--- protected/modules/dataCenter/views/default/_insert_company_type_business.php
+++ protected/modules/dataCenter/views/default/_insert_company_type_business.php
@@ -2,53 +2,61 @@
 $this->renderPartial('_sidebar', array(
     'selectTBusiness' => 'selected',
 ));
+if ($model->id != NULL) {
+    $btnText = 'แก้ไข';
+} else {
+    $btnText = 'เพิ่ม';
+}
 ?>
 
 <div class="content">
     <div class="tabcontents">
         <h3 class="barH3">
             <span>
-                <i class="icon-cog"></i> <a href="<?php echo CHtml::normalizeUrl(array("/member/manage/profile")); ?>">ตั้งค่าเว็บไซต์</a> 
-                <i class="icon-chevron-right"></i><a href="<?php echo CHtml::normalizeUrl(array("/dataCenter/default/companyTypeBusiness")); ?>">ประเภทธุรกิจ</a> 
-                <i class="icon-chevron-right"></i><?php echo Yii::t('language', 'เพิ่มประเภทธุรกิจ '); ?>
+                <i class="icon-cog"></i> 
+                 <a href="<?php echo CHtml::normalizeUrl(array("/member/manage/profile")); ?>">
+                    <?php echo Yii::t('language', 'ตั้งค่าเว็บไซต์'); ?>
+                </a> 
+                <i class="icon-chevron-right"></i> 
+                <a href="<?php echo CHtml::normalizeUrl(array("/dataCenter/default/companyTypeBusiness")); ?>">
+                    <?php echo Yii::t('language', 'ประเภทร้านค้า'); ?>
+                </a> 
+                <i class="icon-chevron-right"></i> 
+                <?php echo Yii::t('language', $btnText) . Yii::t('language', 'ประเภทร้านค้า'); ?>
             </span>
         </h3>
-        <ul class="form _100">
+        <?php
+        $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'insert_company_type_business-form',
+            'focus' => array($model, 'name'),
+        ));
+        ?>
+        <div class="_100">
             <?php
-            $form = $this->beginWidget('CActiveForm', array(
-                'id' => 'insert_company_type_business-form',
-                'focus' => array($model, 'name'),
-            ));
-            if ($model->id != NULL) {
-                $btnText = 'แก้ไข';
-            } else {
-                $btnText = 'เพิ่ม';
-            }
+            echo $form->labelEx($model, 'name');
+            echo $form->textField($model, 'name');
+            echo $form->error($model, 'name')
             ?>
-            <li>
-                <?php
-                echo $form->labelEx($model, 'name');
-                echo $form->textField($model, 'name');
-                echo $form->error($model, 'name')
-                ?>
-            </li>
-            <li>
-                <?php
-                echo $form->labelEx($model, 'name_en');
-                echo $form->textField($model, 'name_en');
-                echo $form->error($model, 'name_en')
-                ?>
-            </li>
-            <li class="textcenter">
-                <?php
-                echo CHtml::submitButton($btnText);
-                echo CHtml::button('ย้อนกลับ', array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
-                        '/dataCenter/default/companyTypeBusiness'
-                    )) . "'")
-                );
-                ?>
-            </li>
-        </ul>
+        </div>
+        <div class="_100">
+            <?php
+            echo $form->labelEx($model, 'name_en');
+            echo $form->textField($model, 'name_en');
+            echo $form->error($model, 'name_en')
+            ?>
+        </div>
+        <div class="textcenter">
+            <hr>
+            <?php
+            echo CHtml::submitButton(Yii::t('language', 'บันทึก'));
+            echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
+                    '/dataCenter/default/companyTypeBusiness'
+                )) . "'")
+            );
+            ?>
+            <hr>
+        </div>
         <?php
         $this->endWidget();
         ?>
