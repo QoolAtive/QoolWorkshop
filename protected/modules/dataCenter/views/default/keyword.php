641fca5ef5e0216d57c3d5bfa4ff462249d6bcc7
annbluesky <ann.kanowan@gmail.com>
2013-09-06 17:13:50 +0700

    Data Center: คำสำคัญ แก้ไสำหรับการแปลภาษา

--- protected/modules/dataCenter/views/default/keyword.php
+++ protected/modules/dataCenter/views/default/keyword.php
@@ -1,30 +1,29 @@
 <?php
 $this->renderPartial('_sidebar', array(
-//    'selectEdu' => '',
-//    'selectTBusiness' => '',
-//    'selectSex' => '',
-//    'slectTname' => 'selected',
+    'selectKey' => 'selected',
 ));
 ?>
-
 <div class="content">
     <div class="tabcontents">
         <h3 class="barH3">
             <span>
                 <i class="icon-cog"></i> 
                 <a href="<?php echo CHtml::normalizeUrl(array("/member/manage/profile")); ?>">
-                    ตั้งค่าเว็บไซต์
+                    <?php echo Yii::t('language', 'ตั้งค่าเว็บไซต์'); ?>
                 </a>
-                <i class="icon-chevron-right"></i><?php echo Yii::t('language', 'Keyword'); ?>
+                <i class="icon-chevron-right"></i> 
+                    <?php echo Yii::t('language', 'คำสำคัญ'); ?>
             </span>
         </h3>
         <div style="text-align: center;">
             <?php
-            echo CHtml::button(Yii::t('language', 'เพิ่ม') . ' ' . Yii::t('language', 'Keyword'), array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+            echo CHtml::button(Yii::t('language', 'เพิ่ม') . ' ' . Yii::t('language', 'คำสำคัญ'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/dataCenter/default/keywordInsert'
                 )) . "'")
             );
             ?>
+            <hr>
         </div>
         <?php
         $this->renderPartial('keyword_grid', array(
@@ -33,12 +32,15 @@ $this->renderPartial('_sidebar', array(
         ));
         ?>
         <div style="text-align: center;">
+            <hr>
             <?php
-            echo CHtml::button('ย้อนกลับ', array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+            echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/member/manage/profile'
                 )) . "'")
             );
             ?>
+            <hr>
         </div>
     </div>
 </div>
--- protected/modules/dataCenter/views/default/keyword_insert.php
+++ protected/modules/dataCenter/views/default/keyword_insert.php
@@ -2,19 +2,29 @@
 $this->renderPartial('_sidebar', array(
     'selectKey' => 'selected',
 ));
+
+if ($model->keyword_id != NULL) {
+    $btnText = 'แก้ไข';
+} else {
+    $btnText = 'เพิ่ม';
+}
 ?>
-
-
-
 <div class="content">
     <div class="tabcontents">
 
 
         <h3 class="barH3">
             <span>
-                <i class="icon-cog"></i> <a href="<?php echo CHtml::normalizeUrl(array("/member/manage/profile")); ?>"><?php echo Yii::t('language', 'ตั้งค่าเว็บไซต์'); ?></a> 
-                <i class="icon-chevron-right"></i><a href="<?php echo CHtml::normalizeUrl(array("/dataCenter/default/keyword")); ?>"><?php echo Yii::t('language', 'Keyword'); ?></a> 
-                <i class="icon-chevron-right"></i><?php echo Yii::t('language', 'เพิ่ม') . ' ' . Yii::t('language', 'Keyword'); ?>
+                <i class="icon-cog"></i> 
+                <a href="<?php echo CHtml::normalizeUrl(array("/member/manage/profile")); ?>">
+                    <?php echo Yii::t('language', 'ตั้งค่าเว็บไซต์'); ?>
+                </a> 
+                <i class="icon-chevron-right"></i> 
+                <a href="<?php echo CHtml::normalizeUrl(array("/dataCenter/default/keyword")); ?>">
+                    <?php echo Yii::t('language', 'คำสำคัญ'); ?>
+                </a> 
+                <i class="icon-chevron-right"></i> 
+                <?php echo Yii::t('language', $btnText) . Yii::t('language', 'คำสำคัญ'); ?>
             </span>
         </h3>
         <?php
@@ -22,11 +32,6 @@ $this->renderPartial('_sidebar', array(
             'id' => 'keyword_insert-form',
             'focus' => array($model, 'name'),
         ));
-        if ($model->keyword_id != NULL) {
-            $btnText = 'แก้ไข';
-        } else {
-            $btnText = 'เพิ่ม';
-        }
         ?>
         <div class="_100">
             <div class="_100">
@@ -37,13 +42,16 @@ $this->renderPartial('_sidebar', array(
                 ?>
             </div>
             <div class="_100" style="text-align: center;">
+                <hr>
                 <?php
-                echo CHtml::submitButton($btnText);
-                echo CHtml::button('ย้อนกลับ', array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+                echo CHtml::submitButton(Yii::t('language', 'บันทึก'));
+                echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array(
+                    'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                         '/dataCenter/default/keyword'
                     )) . "'")
                 );
                 ?>
+                <hr>
             </div>
         </div>
         <?php
