74e3c01b59b67830d780242c11783dd3920e19a1
annbluesky <ann.kanowan@gmail.com>
2013-09-11 11:46:34 +0700

    Data Center: ใส่ Function แปลภาษา เปลี่ยนการแสดงผลเป็น Tab

--- protected/modules/dataCenter/views/default/site_map.php
+++ protected/modules/dataCenter/views/default/site_map.php
@@ -1,9 +1,6 @@
 <?php
 $this->renderPartial('_sidebar', array(
-//    'selectEdu' => '',
-//    'selectTBusiness' => '',
-//    'selectSex' => '',
-//    'slectTname' => 'selected',
+    'selectSiteMap' => 'selected',
 ));
 ?>
 
@@ -15,46 +12,65 @@ $this->renderPartial('_sidebar', array(
                 <a href="<?php echo CHtml::normalizeUrl(array("/member/manage/profile")); ?>">
                     <?php echo Yii::t('language', 'ตั้งค่าเว็บไซต์'); ?>
                 </a>
-                <i class="icon-chevron-right"></i><?php echo Yii::t('language', 'แผนผังเว็บไซต์'); ?>
+                <i class="icon-chevron-right"></i> 
+                <?php echo Yii::t('language', 'แผนผังเว็บไซต์'); ?>
             </span>
         </h3>
+        <?php
+        $tabs = array();
+
+        $tabs[Yii::t('language', 'หมวดหมู่หลัก')] = array(
+            'id' => 'tab01',
+            'content' => $this->renderPartial('site_map_grid', array(
+                'modelSiteMap' => $modelSiteMap,
+                'dataProvider' => $dataProvider,
+                    ), true, false),
+        );
+
+        $tabs[Yii::t('language', 'หมวดหมู่ย่อย')] = array(
+            'id' => 'tab02',
+            'content' => $this->renderPartial('site_map_sub_grid', array(
+                'modelSiteMapSub' => $modelSiteMapSub,
+                'dataProvider' => $dataProviderSub,
+                    ), true, false),
+        );
+        ?>
         <div style="text-align: center;">
             <?php
-            echo CHtml::button('เพิ่มหมวดหมูหลัก', array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+            echo CHtml::button(Yii::t('language', 'เพิ่ม') . Yii::t('language', 'หมวดหมู่หลัก'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/dataCenter/default/siteMapInsert'
                 )) . "'")
             );
-            ?>
-        </div>
-        <h3><?php echo Yii::t('language', 'หัวข้อหลัก'); ?></h3>
-        <?php
-        $this->renderPartial('site_map_grid', array(
-            'modelSiteMap' => $modelSiteMap,
-            'dataProvider' => $dataProvider,
-        ));
-        ?>
-        <h3><?php echo Yii::t('language', 'หัวข้อรอง'); ?></h3>
-        <div style="text-align: center;">
-            <?php
-            echo CHtml::button('เพิ่มหมวดหมูรอง', array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+
+            echo CHtml::button(Yii::t('language', 'เพิ่ม') . Yii::t('language', 'หมวดหมู่ย่อย'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/dataCenter/default/siteMapSubInsert'
                 )) . "'")
             );
             ?>
+            <hr>
         </div>
         <?php
-        $this->renderPartial('site_map_sub_grid', array(
-            'modelSiteMapSub' => $modelSiteMapSub,
-            'dataProvider' => $dataProviderSub,
+        $this->widget('zii.widgets.jui.CJuiTabs', array(
+            'tabs' => $tabs,
+            'options' => array(
+//                'collapsible' => true,
+                'collapsible' => false,
+            ),
+            'id' => 'tab_all',
         ));
-        ?>
+        ?>       
         <div style="text-align: center;">
+            <hr>
             <?php
-            echo CHtml::button('ย้อนกลับ', array('onClick' => "window.location='" . CHtml::normalizeUrl(array(
+            echo CHtml::button(Yii::t('language', 'ย้อนกลับ'), array(
+                'onClick' => "window.location='" . CHtml::normalizeUrl(array(
                     '/member/manage/profile'
                 )) . "'")
             );
             ?>
+            <hr>
         </div>
     </div>
 </div>
