53397380afe57857a70d9966becf6f37580d0f61
annbluesky <ann.kanowan@gmail.com>
2013-09-11 11:48:49 +0700

    Data Center:  แก้ไข หน้า Gridview ส่วนหมวดหมู่ย่อย + ใส่ Function แปลภาษา

--- protected/modules/dataCenter/views/default/site_map_sub_grid.php
+++ protected/modules/dataCenter/views/default/site_map_sub_grid.php
@@ -4,11 +4,11 @@ $this->widget('zii.widgets.grid.CGridView', array(
     'id' => 'site_map_sub-grid',
     'dataProvider' => $dataProvider,
     'filter' => $modelSiteMapSub,
-    'ajaxUpdate' => true,
     'summaryText' => '',
+    'emptyText' => Yii::t('language', 'ไม่พบข้อมูล'),
     'columns' => array(
-        array(// display 'create_time' using an expression
-            'header' => 'ลำดับ',
+        array(
+            'header' => Yii::t('language', 'ลำดับ'),
             'headerHtmlOptions' => array('style' => 'width: 7%;'),
             'htmlOptions' => array('style' => 'text-align: center;'),
             'value' => '$this->grid->dataProvider->pagination->currentPage * $this->grid->dataProvider->pagination->pageSize + ($row+1)."."',
@@ -31,15 +31,15 @@ $this->widget('zii.widgets.grid.CGridView', array(
         array(
             'class' => 'CButtonColumn',
             'header' => Yii::t('language', 'เครื่องมือ'),
-            'deleteConfirmation' => 'คุณต้องการลบบทความหรือไม่?',
-            'template' => '{update}{delete}',
+            'deleteConfirmation' => Yii::t('language', 'คุณต้องการลบข้อมูลนี้หรือไม่?'),
+            'template' => '{update}&nbsp;&nbsp;{delete}',
             'buttons' => array(
                 'update' => array(
-                    'label' => 'edit', //Text label of the button.
+                    'label' => Yii::t('language', 'แก้ไข'),
                     'url' => 'Yii::app()->createUrl("/dataCenter/default/siteMapSubInsert/",array("site_map_sub_id"=>$data->site_map_sub_id))',
                 ),
                 'delete' => array(
-                    'label' => 'del', //Text label of the button.
+                    'label' => Yii::t('language', 'ลบ'),
                     'url' => 'Yii::app()->createUrl("/dataCenter/default/siteMapSubDel",array("site_map_sub_id"=>$data->site_map_sub_id))',
                 ),
             ),
@@ -50,13 +50,14 @@ $this->widget('zii.widgets.grid.CGridView', array(
                     }'
         ),
     ),
+    'template' => "{items}\n{pager}",
     'pager' => array(
         'class' => 'CLinkPager',
-        'header' => 'หน้าที่: ',
-        'firstPageLabel' => 'หน้าแรก',
-        'prevPageLabel' => 'ก่อนหน้า',
-        'nextPageLabel' => 'หน้าถัดไป',
-        'lastPageLabel' => 'หน้าสุดท้าย',
+        'header' => Yii::t('language', 'หน้าที่: '),
+        'firstPageLabel' => Yii::t('language', 'หน้าแรก'),
+        'prevPageLabel' => Yii::t('language', 'ก่อนหน้า'),
+        'nextPageLabel' => Yii::t('language', 'หน้าถัดไป'),
+        'lastPageLabel' => Yii::t('language', 'หน้าสุดท้าย'),
     )
 ));
 ?>
