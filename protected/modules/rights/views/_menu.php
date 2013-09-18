3061d99b33abfeedb7989a6216199da4e1f667ab
annbluesky <ann.kanowan@gmail.com>
2013-09-18 14:10:49 +0700

    Rights: แก้ไขคำสำหรับแปลภาษา ส่วน menu

--- protected/modules/rights/views/_menu.php
+++ protected/modules/rights/views/_menu.php
@@ -4,33 +4,33 @@
 	'htmlOptions'=>array('class'=>'actions'),
 	'items'=>array(
 		array(
-			'label'=>Rights::t('core', 'Assignments'),
+			'label'=>Rights::t('core', 'มอบหมาย'),// Assignments
 			'url'=>array('assignment/view'),
 			'itemOptions'=>array('class'=>'item-assignments'),
 		),
 		array(
-			'label'=>Rights::t('core', 'Permissions'),
+			'label'=>Rights::t('core', 'สิทธิ์'),//Permissions
 			'url'=>array('authItem/permissions'),
 			'itemOptions'=>array('class'=>'item-permissions'),
 		),
 		array(
-			'label'=>Rights::t('core', 'Roles'),
+			'label'=>Rights::t('core', 'หน้าที่'),//Roles
 			'url'=>array('authItem/roles'),
 			'itemOptions'=>array('class'=>'item-roles'),
 		),
 		array(
-			'label'=>Rights::t('core', 'Tasks'),
+			'label'=>Rights::t('core', 'งาน'),//Tasks
 			'url'=>array('authItem/tasks'),
 			'itemOptions'=>array('class'=>'item-tasks'),
 		),
 		array(
-			'label'=>Rights::t('core', 'Operations'),
+			'label'=>Rights::t('core', 'การดำเนินการ'),//Operations
 			'url'=>array('authItem/operations'),
 			'itemOptions'=>array('class'=>'item-operations'),
 		),
 
 		array(
-			'label'=>Rights::t('core', 'ย้อนกลับ'),
+			'label'=>Rights::t('core', 'ย้อนกลับ'),//
 			'url'=>array('/member/manage/profile'),
 			'itemOptions'=>array('class'=>'item-operations'),
 		),
