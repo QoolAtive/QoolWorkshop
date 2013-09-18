8698e7b4967ebece3b6c7b807392ee60b48b914c
annbluesky <ann.kanowan@gmail.com>
2013-09-18 10:48:07 +0700

    About Us: แก้ไขหน้า form about

--- protected/modules/about/views/default/edit_about.php
+++ protected/modules/about/views/default/edit_about.php
@@ -1,133 +1,88 @@
-<div class="sidebar">
-    <div class="menuitem">
-        <ul>
-            <li class="boxhead"><img src="<?php echo Yii::t('language', '/img/iconpage/about.png'); ?>"/></li>
-        </ul>
-        <ul class="tabs clearfix">
-            <li class="noselect">
-                <?php
-                echo CHtml::link(Yii::t('language', 'เกี่ยวกับเรา'), CHtml::normalizeUrl(
-                                array('/about/default/index/view/1')
-                        ), array('rel' => 'view1')
-                );
-                ?>
-            </li>
-            <li class="noselect">
-                <?php
-                echo CHtml::link(
-                        Yii::t('language', 'ติดต่อเรา'), CHtml::normalizeUrl(
-                                array('/about/default/index/view/2')
-                        ), array('rel' => 'view2')
-                );
-                ?>
-            </li>
-            <li class="noselect">
-                <?php
-                echo CHtml::link(
-                        Yii::t('language', 'Sitemap'), CHtml::normalizeUrl(
-                                array('/about/default/siteMap')
-                        ), array('rel' => 'view3')
-                );
-                ?>
-            </li>
+<?php
+$this->renderPartial('_menu', array('select' => 4));
+?>
+<div class="content">
+
+    <div id="text" class="row-fluid tabcontents">
+        <h3 class="barH3">        
+            <span>
+                <i class="icon-home"></i>
+                <a href="<?php echo CHtml::normalizeUrl(array("/about/default/index/view/1")); ?>">
+                    <?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'เกี่ยวกับเรา'); ?>
+                </a>
+                <i class="icon-chevron-right"></i>
+                <?php echo Yii::t('language', 'แก้ไข') . Yii::t('language', 'เกี่ยวกับเรา'); ?>
+            </span>
+        </h3>
+        <?php
+        $model = About::model()->find();
+        $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'insert-form',
+        ));
+
+        echo $form->errorSummary($model);
+        ?>
+        <div class="_100">
             <?php
-            if (Yii::app()->user->isAdmin()) {
-                ?>
-                <li class="selected">
-                    <?php
-                    echo CHtml::link(
-                            Yii::t('language', 'จัดการ') . "<br/>" . Yii::t('language', 'เกี่ยวกับเรา'), CHtml::normalizeUrl(
-                                    array('/about/default/editAbout')
-                            ), array('rel' => 'view3')
-                    );
-                    ?>
-                </li>
-            <?php } ?>
-        </ul>
+            //ภาษาไทย
+            echo $form->labelEx($model, 'about_text_th');
+            $this->widget('ext.ckeditor.CKEditorWidget', array(
+                "model" => $model, # Data-Model
+                "attribute" => 'about_text_th', # Attribute in the Data-Model
+                "defaultValue" => $model->about_text_th, # Optional
+                "config" => array(
+                    "height" => "500px",
+                    "width" => "100%",
+                    'toolbar' => array(
+                        array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
+                            '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
+                            '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
+                        array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
+                            '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
+                            '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About'),
+                    ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
+                ),
+                "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
+                # Path to ckeditor.php
+                "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
+            ));
+            ?>
+        </div>
+        <div class="_100">
+            <?php
+            //ภาษาอังกฤษ
+            echo $form->labelEx($model, 'about_text_en');
+            $this->widget('ext.ckeditor.CKEditorWidget', array(
+                "model" => $model, # Data-Model
+                "attribute" => 'about_text_en', # Attribute in the Data-Model
+                "defaultValue" => $model->about_text_en, # Optional
+                "config" => array(
+                    "height" => "500px",
+                    "width" => "100%",
+                    'toolbar' => array(
+                        array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
+                            '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
+                            '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
+                        array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
+                            '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
+                            '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About'),
+                    ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
+                ),
+                "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
+                # Path to ckeditor.php
+                "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
+            ));
+            ?>
+        </div>
+        <div class="txt-cen">
+            <hr>
+            <?php
+            echo CHtml::submitButton(Yii::t('language', 'บันทึก'));
+            echo CHtml::button(Yii::t('language', 'ยกเลิก'), array(
+                'onclick' => 'window.location = "' . CHtml::normalizeUrl(array("/about/default/index/view/1")) . '"'));
+            ?>
+            <hr>
+        </div>
+        <?php $this->endWidget(); ?>
     </div>
 </div>
-<div class="content">
-    
-        <div id="text" class="row-fluid tabcontents">
-            <h3 class="barH3">        
-                <span>
-                    <i class="icon-home"></i>
-                    <a href="<?php echo CHtml::normalizeUrl(array("/about/default/index/view/1")); ?>">
-                        <?php echo Yii::t('language', 'จัดการ') . Yii::t('language', 'เกี่ยวกับเรา'); ?>
-                    </a>
-                    <i class="icon-chevron-right"></i>
-                    <?php echo Yii::t('language', 'แก้ไข') . Yii::t('language', 'เกี่ยวกับเรา'); ?>
-                </span>
-            </h3>
-            <?php
-            $model = About::model()->find();
-            $form = $this->beginWidget('CActiveForm', array(
-                'id' => 'insert-form',
-            ));
-
-            echo $form->errorSummary($model);
-            ?>
-            <div class="_100">
-                <?php
-                //ภาษาไทย
-                echo $form->labelEx($model, 'about_text_th');
-                $this->widget('ext.ckeditor.CKEditorWidget', array(
-                    "model" => $model, # Data-Model
-                    "attribute" => 'about_text_th', # Attribute in the Data-Model
-                    "defaultValue" => $model->about_text_th, # Optional
-                    "config" => array(
-                        "height" => "500px",
-                        "width" => "100%",
-                        'toolbar' => array(
-                            array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
-                                '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
-                                '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
-                            array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
-                                '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
-                                '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About'),
-                        ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
-                    ),
-                    "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
-                    # Path to ckeditor.php
-                    "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
-                ));
-                ?>
-            </div>
-            <div class="_100">
-                <?php
-                //ภาษาอังกฤษ
-                echo $form->labelEx($model, 'about_text_en');
-                $this->widget('ext.ckeditor.CKEditorWidget', array(
-                    "model" => $model, # Data-Model
-                    "attribute" => 'about_text_en', # Attribute in the Data-Model
-                    "defaultValue" => $model->about_text_en, # Optional
-                    "config" => array(
-                        "height" => "500px",
-                        "width" => "100%",
-                        'toolbar' => array(
-                            array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
-                                '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
-                                '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
-                            array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
-                                '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
-                                '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About'),
-                        ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
-                    ),
-                    "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
-                    # Path to ckeditor.php
-                    "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
-                ));
-                ?>
-            </div>
-            <div class="txt-cen">
-                <hr>
-                <?php
-                echo CHtml::submitButton(Yii::t('language', 'บันทึก'));
-                echo CHtml::button(Yii::t('language', 'ยกเลิก'), array(
-                    'onclick' => 'window.location = "' . CHtml::normalizeUrl(array("/about/default/index/view/1")) . '"'));
-                ?>
-                <hr>
-            </div>
-            <?php $this->endWidget(); ?>
-        </div>
-    </div>
