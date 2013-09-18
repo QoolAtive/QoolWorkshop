69d281196ef1ae08831c1ad5ff3ea0e2a7b5b609
annbluesky <ann.kanowan@gmail.com>
2013-09-18 10:47:19 +0700

    About Us: แก้ไขหน้า site map เพิ่ม Function เปลี่ยนภาษา

--- protected/modules/about/views/default/site_map.php
+++ protected/modules/about/views/default/site_map.php
@@ -1,61 +1,21 @@
-<div class="sidebar">
-    <div class="menuitem">
-        <ul>
-            <li class="boxhead"><img src="<?php echo Yii::t('language', '/img/iconpage/about.png'); ?>"/></li>
-        </ul>
-        <ul class="tabs clearfix">
-            <li class="<?php echo $select1; ?>">
-                <?php
-                echo CHtml::link(Yii::t('language', 'เกี่ยวกับเรา'), CHtml::normalizeUrl(
-                                array('/about/default/index/view/1')
-                        ), array('rel' => 'view1')
-                );
-                ?>
-            </li>
-            <li class="<?php echo $select2; ?>">
-                <?php
-                echo CHtml::link(
-                        Yii::t('language', 'ติดต่อเรา'), CHtml::normalizeUrl(
-                                array('/about/default/index/view/2')
-                        ), array('rel' => 'view2')
-                );
-                ?>
-            </li>
-            <li class="<?php echo $select3; ?>">
-                <?php
-                echo CHtml::link(
-                        Yii::t('language', 'Sitemap'), CHtml::normalizeUrl(
-                                array('/about/default/index/view/3')
-                        ), array('rel' => 'view3')
-                );
-                ?>
-            </li>
-            <?php
-            if (Yii::app()->user->isAdmin()) {
-                ?>
-                <li>
-                    <?php
-                    echo CHtml::link(
-                            Yii::t('language', 'จัดการ') . "<br/>" . Yii::t('language', 'เกี่ยวกับเรา'), CHtml::normalizeUrl(
-                                    array('/about/default/editAbout')
-                            ), array('rel' => 'view3')
-                    );
-                    ?>
-                </li>
-            <?php } ?>
-        </ul>
-    </div>
-</div>
+<?php
+$this->renderPartail('_menu', array('select' => 3));
+?>
 <div class="content">
     <div class="tabcontents">
         <div class="sitemap">
 
             <img class="pagebanner" alt="pagebanner" src="/img/banner/about.png">
-            <h2 class="textcenter"><strong>แผนผังเว็บไซต์ DBD Mart</strong></h2>
+            <h2 class="textcenter"><strong><?php echo Yii::t('language', 'แผนที่เว็บไซต์'); ?>DBD Mart</strong></h2>
             <?php
             foreach ($modelSiteMapMain as $main) {
                 ?>
-                <p><?php echo CHtml::link($main->name, array($main->link), array('target' => '_bank')); ?></p>
+                <p>
+                    <?php
+                    $main_name = LanguageHelper::changeDB($main->name, $main->name_en);
+                    echo CHtml::link($main_name, array($main->link), array('target' => '_bank'));
+                    ?>
+                </p>
                 <?php
                 $cSub = new CDbCriteria();
                 $cSub->condition = 'main_id = :main_id and sub_id is null';
@@ -68,7 +28,12 @@
                         <?php
                         foreach ($modelSiteMapSub as $sub) {
                             ?>
-                            <li><?php echo CHtml::link($sub->name, array($sub->link), array('target' => '_bank')); ?></li>
+                            <li>
+                                <?php
+                                $sub_name = LanguageHelper::changeDB($sub->name, $sub->name_en);
+                                echo CHtml::link($sub_name, array($sub->link), array('target' => '_bank'));
+                                ?>
+                            </li>
                             <?php
                         }
                         ?>
