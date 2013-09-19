a123fb1fe3edfaf9d392a292264408cb481ff34b
annbluesky <ann.kanowan@gmail.com>
2013-09-19 17:54:11 +0700

    Web Simulation: แก้ไขหน้า edit_how_to_buy

--- protected/modules/webSimulation/views/manageShop/edit_how_to_buy.php
+++ protected/modules/webSimulation/views/manageShop/edit_how_to_buy.php
@@ -7,7 +7,7 @@ $this->renderPartial('_side_menu', array('index' => 'shop'));
             <span>
                 <i class="icon-shopping-cart"></i>
                 <a href="<?php echo CHtml::normalizeUrl(array("/webSimulation/manageShop/manageShop")); ?>">
-                    <?php echo Yii::t('language', 'ร้าน ') . $model->name_th; ?>
+                    <?php echo Yii::t('language', 'ร้าน :n', array(':n' => $model->name_th)); ?>
                 </a>
                 <i class="icon-chevron-right"></i>
                 <a href="<?php echo CHtml::normalizeUrl(array("/webSimulation/manageShop/manageShop")); ?>">
@@ -19,73 +19,73 @@ $this->renderPartial('_side_menu', array('index' => 'shop'));
         </h3>
 
         <?php
-            $form = $this->beginWidget('CActiveForm', array(
-                'id' => 'how_to_buy-form',
-            ));
+        $form = $this->beginWidget('CActiveForm', array(
+            'id' => 'how_to_buy-form',
+        ));
 
-            echo $form->errorSummary($model);
+        echo $form->errorSummary($model);
+        ?>
+        <div class="_100">
+            <?php
+            //ภาษาไทย
+            echo $form->labelEx($model, 'how_to_buy_th');
+            $this->widget('ext.ckeditor.CKEditorWidget', array(
+                "model" => $model, # Data-Model
+                "attribute" => 'how_to_buy_th', # Attribute in the Data-Model
+                "defaultValue" => $model->how_to_buy_th, # Optional
+                "config" => array(
+                    "height" => "500px",
+                    "width" => "100%",
+                    'toolbar' => array(
+                        array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
+                            '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
+                            '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
+                        array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
+                            '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
+                            '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About'),
+                    ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
+                ),
+                "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
+                # Path to ckeditor.php
+                "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
+            ));
             ?>
-            <div class="_100">
-                <?php
-                //ภาษาไทย
-                echo $form->labelEx($model, 'how_to_buy_th');
-                $this->widget('ext.ckeditor.CKEditorWidget', array(
-                    "model" => $model, # Data-Model
-                    "attribute" => 'how_to_buy_th', # Attribute in the Data-Model
-                    "defaultValue" => $model->how_to_buy_th, # Optional
-                    "config" => array(
-                        "height" => "500px",
-                        "width" => "100%",
-                        'toolbar' => array(
-                            array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
-                                '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
-                                '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
-                            array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
-                                '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
-                                '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About'),
-                        ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
-                    ),
-                    "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
-                    # Path to ckeditor.php
-                    "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
-                ));
-                ?>
-            </div>
-            <div class="_100">
-                <?php
-                //ภาษาอังกฤษ
-                echo $form->labelEx($model, 'how_to_buy_en');
-                $this->widget('ext.ckeditor.CKEditorWidget', array(
-                    "model" => $model, # Data-Model
-                    "attribute" => 'how_to_buy_en', # Attribute in the Data-Model
-                    "defaultValue" => $model->how_to_buy_en, # Optional
-                    "config" => array(
-                        "height" => "500px",
-                        "width" => "100%",
-                        'toolbar' => array(
-                            array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
-                                '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
-                                '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
-                            array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
-                                '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
-                                '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About'),
-                        ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
-                    ),
-                    "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
-                    # Path to ckeditor.php
-                    "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
-                ));
-                ?>
-            </div>
-            <div class="txt-cen">
-                <hr>
-                <?php
-                echo CHtml::submitButton(Yii::t('language', 'บันทึก'));
-                echo CHtml::button(Yii::t('language', 'ยกเลิก'), array(
-                    'onclick' => 'window.location = "' . CHtml::normalizeUrl(array("/webSimulation/manageShop/manageShop")) . '"'));
-                ?>
-                <hr>
-            </div>
-            <?php $this->endWidget(); ?>
+        </div>
+        <div class="_100">
+            <?php
+            //ภาษาอังกฤษ
+            echo $form->labelEx($model, 'how_to_buy_en');
+            $this->widget('ext.ckeditor.CKEditorWidget', array(
+                "model" => $model, # Data-Model
+                "attribute" => 'how_to_buy_en', # Attribute in the Data-Model
+                "defaultValue" => $model->how_to_buy_en, # Optional
+                "config" => array(
+                    "height" => "500px",
+                    "width" => "100%",
+                    'toolbar' => array(
+                        array('Font', 'FontSize', '-', 'Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript',
+                            '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',
+                            '-', 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', 'Blockquote'),
+                        array('TextColor', 'BGColor', '-', 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo',
+                            '-', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak',
+                            '-', 'Source', '-', 'Link', 'Unlink', '-', 'Maximize', '-', 'About'),
+                    ), # EXISTING(!) Toolbar (see: ckeditor.js) Ex. "toolbar" => "Basic"
+                ),
+                "ckEditor" => Yii::app()->basePath . "/../js/ckeditor/ckeditor.php",
+                # Path to ckeditor.php
+                "ckBasePath" => Yii::app()->baseUrl . "/js/ckeditor/",
+            ));
+            ?>
+        </div>
+        <div class="_100 txt-cen">
+            <hr>
+            <?php
+            echo CHtml::submitButton(Yii::t('language', 'บันทึก'));
+            echo CHtml::button(Yii::t('language', 'ยกเลิก'), array(
+                'onclick' => 'window.location = "' . CHtml::normalizeUrl(array("/webSimulation/manageShop/manageShop")) . '"'));
+            ?>
+            <hr>
+        </div>
+        <?php $this->endWidget(); ?>
     </div>
 </div>
