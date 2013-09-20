60ea9553851abc4321621dec581e8f60a7784fea
annbluesky <ann.kanowan@gmail.com>
2013-09-20 13:39:52 +0700

    Web Simulation: แก้ไขแปลภาษา Model webShopBox

--- protected/models/WebShopBox.php
+++ protected/models/WebShopBox.php
@@ -17,96 +17,91 @@
  * @property WebShop $webShop
  * @property WebShopBoxItem[] $webShopBoxItems
  */
-class WebShopBox extends WebShopBoxBase
-{
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return WebShopBoxBase the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+class WebShopBox extends WebShopBoxBase {
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'web_shop_box';
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return WebShopBoxBase the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('web_shop_id, name_th, name_en, type, order_n, show_box', 'required'),
-			array('web_shop_id, type, order_n, show_box', 'numerical', 'integerOnly'=>true),
-			array('name_th, name_en', 'length', 'max'=>100),
-			array('code', 'safe'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('web_shop_box_id, web_shop_id, name_th, name_en, type, code, order_n, show_box', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'web_shop_box';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'webShop' => array(self::BELONGS_TO, 'WebShop', 'web_shop_id'),
-			'webShopBoxItems' => array(self::HAS_MANY, 'WebShopBoxItem', 'web_shop_box_id'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('web_shop_id, name_th, name_en, type, order_n, show_box', 'required'),
+            array('web_shop_id, type, order_n, show_box', 'numerical', 'integerOnly' => true),
+            array('name_th, name_en', 'length', 'max' => 100),
+            array('code', 'safe'),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('web_shop_box_id, web_shop_id, name_th, name_en, type, code, order_n, show_box', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'web_shop_box_id' => 'Web Shop Box',
-			'web_shop_id' => 'Web Shop',
-			'name_th' => 'ชื่อกล่องภาษาไทย',
-			'name_en' => 'ชื่อกล่องภาษาอังกฤษ',
-			'type' => 'Type',
-			'code' => 'Code',
-			'order_n' => 'Order N',
-			'show_box' => 'Show',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'webShop' => array(self::BELONGS_TO, 'WebShop', 'web_shop_id'),
+            'webShopBoxItems' => array(self::HAS_MANY, 'WebShopBoxItem', 'web_shop_box_id'),
+        );
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'web_shop_box_id' => 'Web Shop Box',
+            'web_shop_id' => 'Web Shop',
+            'name_th' => Yii::t('language', 'ชื่อกล่อง') . ' (' . Yii::t('language', 'ภาษาไทย') . ')',
+            'name_en' => Yii::t('language', 'ชื่อกล่อง') . ' (' . Yii::t('language', 'ภาษาอังกฤษ') . ')',
+            'type' => 'Type',
+            'code' => 'Code',
+            'order_n' => 'Order N',
+            'show_box' => 'Show',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search() {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
 
-		$criteria->compare('web_shop_box_id',$this->web_shop_box_id);
-		$criteria->compare('web_shop_id',$this->web_shop_id);
-		$criteria->compare('name_th',$this->name_th,true);
-		$criteria->compare('name_en',$this->name_en,true);
-		$criteria->compare('type',$this->type);
-		$criteria->compare('code',$this->code,true);
-		$criteria->compare('order_n',$this->order_n);
-		$criteria->compare('show_box',$this->show_box);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria->compare('web_shop_box_id', $this->web_shop_box_id);
+        $criteria->compare('web_shop_id', $this->web_shop_id);
+        $criteria->compare('name_th', $this->name_th, true);
+        $criteria->compare('name_en', $this->name_en, true);
+        $criteria->compare('type', $this->type);
+        $criteria->compare('code', $this->code, true);
+        $criteria->compare('order_n', $this->order_n);
+        $criteria->compare('show_box', $this->show_box);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
 }
